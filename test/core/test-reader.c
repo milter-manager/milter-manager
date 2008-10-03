8906664ac3643946b9ee24bd9af86f0f8b4c55de
ikezoe <ikezoe@2eb25379-3f72-4156-9665-29aef8dc9b31>
2008-10-03 07:06:44 +0000

    * test/core/test-reader.c: Added GIOChannel test.

--- test/core/test-reader.c
+++ test/core/test-reader.c
@@ -18,27 +18,33 @@
  */
 
 #include <gcutter.h>
+#include <glib/gstdio.h>
 
 #define shutdown inet_shutdown
 #include <milter-core/milter-reader.h>
 #undef shutdown
 
 void test_reader_string (void);
+void test_reader_io_channel (void);
 
 static MilterReader *reader;
 
 static GString *actual_string;
-static GIOChannel *io_channel;
+static GIOChannel *read_channel;
+static GIOChannel *write_channel;
 
 static GError *expected_error;
 static GError *actual_error;
+static gchar *temporary_name;
 
 void
 setup (void)
 {
     reader = NULL;
 
-    actual_string = g_string_new(NULL);
+    actual_string = NULL;
+    read_channel = NULL;
+    write_channel = NULL;
 
     expected_error = NULL;
     actual_error = NULL;
@@ -53,11 +59,17 @@ teardown (void)
 
     if (actual_string)
         g_string_free(actual_string, TRUE);
+    if (read_channel)
+        g_io_channel_unref(read_channel);
+    if (write_channel)
+        g_io_channel_unref(write_channel);
 
     if (expected_error)
         g_error_free(expected_error);
     if (actual_error)
         g_error_free(actual_error);
+    if (g_file_test(temporary_name, G_FILE_TEST_EXISTS))
+        g_unlink(temporary_name);
 }
 
 void
@@ -66,12 +78,61 @@ test_reader_string (void)
     gsize read_size;
     gchar *expected_string = NULL;
 
-    actual_string = g_string_append(actual_string, "first");
+    actual_string = g_string_new("first");
     reader = milter_reader_string_new(actual_string);
 
     milter_reader_read(reader, &expected_string, &read_size,
                        &actual_error);
-    expected_string = cut_take_string(expected_string);
+    expected_string = (gchar *)cut_take_string(expected_string);
+    cut_assert_equal_string("first", expected_string);
+    cut_assert_equal_uint(sizeof("first") - 1, read_size);
+    gcut_assert_error(actual_error);
+}
+
+static void
+make_temporary_file (void)
+{
+    gint fd;
+
+    temporary_name = (gchar *)cut_take_string(g_strdup("test_readerXXXXXX"));
+    fd = g_mkstemp(temporary_name);
+    if (fd == -1)
+        return;
+
+    read_channel = g_io_channel_unix_new(fd);
+    g_io_channel_set_close_on_unref(read_channel, TRUE);
+
+    write_channel = g_io_channel_new_file(temporary_name, "w", NULL);
+}
+
+static void
+write_to_io_channel (const gchar *data)
+{
+    gsize bytes_written;
+
+    g_io_channel_write_chars(write_channel,
+                             data, strlen(data),
+                             &bytes_written,
+                             NULL);
+    g_io_channel_flush(write_channel, NULL);
+}
+
+void
+test_reader_io_channel (void)
+{
+    gsize read_size;
+    gchar *expected_string = NULL;
+
+    make_temporary_file();
+    cut_assert_not_null(read_channel);
+    cut_assert_not_null(write_channel);
+
+    reader = milter_reader_io_channel_new(read_channel);
+
+    write_to_io_channel("first");
+    milter_reader_read(reader, &expected_string, &read_size,
+                       &actual_error);
+    expected_string = (gchar *)cut_take_string(expected_string);
     cut_assert_equal_string("first", expected_string);
     cut_assert_equal_uint(sizeof("first") - 1, read_size);
     gcut_assert_error(actual_error);
