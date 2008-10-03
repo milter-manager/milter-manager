1d9ca59b2191ffd747131976d00a6386ce8eeae7
ktou <ktou@2eb25379-3f72-4156-9665-29aef8dc9b31>
2008-10-03 00:12:52 +0000

    * **/Makefile.am, enum-types.mk, marshal.mk: share enum types and   marshalers generation code.

---
+++ marshal.mk
@@ -0,0 +1,15 @@
+# -*- Mode: Makefile; tab-width: 8; indent-tabs-mode: t; -*-
+
+marshal_sources =			\
+	$(marshal_source_prefix).c	\
+	$(marshal_source_prefix).h
+
+EXTRA_DIST += $(marshal_source_prefix).list
+BUILT_SOURCES += $(marshal_sources)
+MAINTAINERCLEANFILES +=	$(marshal_sources)
+
+$(marshal_source_prefix).h: $(marshal_source_prefix).list
+	$(GLIB_GENMARSHAL) $(srcdir)/$(marshal_source_prefix).list --header --prefix=$(marshal_prefix) > $@
+
+$(marshal_source_prefix).c: $(marshal_source_prefix).list
+	$(GLIB_GENMARSHAL) $(srcdir)/$(marshal_source_prefix).list --header --body --prefix=$(marshal_prefix) > $@
