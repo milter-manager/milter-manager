53e3590ff32c8ece7ff7b929776a63f8e980f65e
ktou <ktou@2eb25379-3f72-4156-9665-29aef8dc9b31>
2008-10-03 00:49:20 +0000

    * Makefile.am, configure.ac, milter-manager/: detect Ruby and add   empty milter manager implementation.

---
+++ milter-manager/milter-manager.h
@@ -0,0 +1,34 @@
+/* -*- Mode: C; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ *  Copyright (C) 2008  Kouhei Sutou <kou@cozmixng.org>
+ *
+ *  This library is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef __MILTER_MANAGER_H__
+#define __MILTER_MANAGER_H__
+
+#include <milter-manager/milter-manager-context.h>
+#include <milter-manager/milter-manager-enum-types.h>
+
+G_BEGIN_DECLS
+
+G_END_DECLS
+
+#endif /* __MILTER_MANAGER_H__ */
+
+/*
+vi:nowrap:ai:expandtab:sw=4
+*/
