02abbe287c5411a83a7c07ab723d9ab1ef8a2a8f
ikezoe <ikezoe@2eb25379-3f72-4156-9665-29aef8dc9b31>
2008-10-23 22:44:16 +0000

    * test/manager/test-child-milter.c: Exit error test.

--- test/manager/test-child-milter.c
+++ test/manager/test-child-milter.c
@@ -25,15 +25,28 @@
 #undef shutdown
 
 void test_start (void);
+void test_exit_error (void);
 
 static MilterManagerChildMilter *milter;
-static GError *error;
+static GError *actual_error;
+static GError *expected_error;
+static gboolean error_occured;
+
+static void
+cb_error (MilterErrorEmitable *emitable,
+          GError *error, gpointer user_data)
+{
+    actual_error = g_error_copy(error);
+    error_occured = TRUE;
+}
 
 void
 setup (void)
 {
     milter = milter_manager_child_milter_new("test-milter");
-    error = NULL;
+    actual_error = NULL;
+    expected_error = NULL;
+    error_occured = FALSE;
 }
 
 void
@@ -41,16 +54,36 @@ teardown (void)
 {
     if (milter)
         g_object_unref(milter);
+    if (actual_error)
+        g_error_free(actual_error);
+    if (expected_error)
+        g_error_free(expected_error);
 }
 
 void
 test_start (void)
 {
+    GError *error = NULL;
     g_object_set(milter,
                  "command", "/bin/echo -n",
                  NULL);
     milter_manager_child_milter_start(milter, &error);
     gcut_assert_error(error);
+}
+
+void
+test_exit_error (void)
+{
+    expected_error = g_error_new(MILTER_MANAGER_CHILD_MILTER_ERROR,
+                                 MILTER_MANAGER_CHILD_MILTER_ERROR_MILTER_EXIT,
+                                 "test-milter exits with status: %d", 0);
+    g_signal_connect(milter, "error", G_CALLBACK(cb_error), NULL);
+
+    cut_trace(test_start());
+    while (!error_occured)
+        g_main_context_iteration(NULL, TRUE);
+
+    gcut_assert_equal_error(expected_error, actual_error);
 }
 
 /*
