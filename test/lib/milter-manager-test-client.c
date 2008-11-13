a06f1c15dc6cfc82be6f97ac8af8670a4c95ebb4
ikezoe <ikezoe@2eb25379-3f72-4156-9665-29aef8dc9b31>
2008-11-13 02:43:24 +0000

    * test/lib/milter-manager-test-client.c 	(milter_manager_test_client_wair_reply): reverse return value of 	getter.

--- test/lib/milter-manager-test-client.c
+++ test/lib/milter-manager-test-client.c
@@ -847,7 +847,7 @@ milter_manager_test_client_wait_reply (MilterManagerTestClient *client,
 
     timeout_waiting_id = g_timeout_add(100, cb_timeout_waiting,
                                        &timeout_waiting);
-    while (timeout_waiting && getter(client))
+    while (timeout_waiting && !getter(client))
         g_main_context_iteration(NULL, TRUE);
     g_source_remove(timeout_waiting_id);
 
