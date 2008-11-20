c67b8962832e06684f8f79a3650489d4832e17f4
ktou <ktou@2eb25379-3f72-4156-9665-29aef8dc9b31>
2008-11-20 08:07:27 +0000

    * test/manager/test-controller.c: cleanup.

--- test/manager/test-controller.c
+++ test/manager/test-controller.c
@@ -40,22 +40,20 @@ static MilterWriter *writer;
 
 static MilterManagerControlCommandEncoder *encoder;
 
-static gchar *tmp_dir;
+static gchar *packet;
+static gsize packet_size;
 
-void
-setup (void)
+static gchar *tmp_dir;
+static gchar *custom_config_path;
+
+static void
+setup_io (void)
 {
     GIOChannel *channel;
     MilterReader *reader;
-    MilterEncoder *_encoder;
-
-    config = milter_manager_configuration_new(NULL);
-    controller = milter_manager_controller_new(config);
-
 
     channel = gcut_io_channel_string_new(NULL);
     g_io_channel_set_encoding(channel, NULL, NULL);
-    g_io_channel_set_flags(channel, G_IO_FLAG_NONBLOCK, NULL);
     gcut_string_io_channel_set_pipe_mode(channel, TRUE);
 
     reader = milter_reader_io_channel_new(channel);
@@ -65,7 +63,17 @@ setup (void)
     writer = milter_writer_io_channel_new(channel);
 
     g_io_channel_unref(channel);
+}
 
+void
+setup (void)
+{
+    MilterEncoder *_encoder;
+
+    config = milter_manager_configuration_new(NULL);
+    controller = milter_manager_controller_new(config);
+
+    setup_io();
 
     _encoder = milter_manager_control_command_encoder_new();
     encoder = MILTER_MANAGER_CONTROL_COMMAND_ENCODER(_encoder);
@@ -73,12 +81,19 @@ setup (void)
     expected_error = NULL;
     actual_error = NULL;
 
+    packet = NULL;
+    packet_size = 0;
 
     tmp_dir = g_build_filename(milter_manager_test_get_base_dir(),
                                "tmp",
                                NULL);
     if (g_mkdir_with_parents(tmp_dir, 0700) == -1)
         cut_assert_errno();
+    milter_manager_configuration_add_load_path(config, tmp_dir);
+
+    custom_config_path = g_build_filename(tmp_dir,
+                                          CUSTOM_CONFIG_FILE_NAME,
+                                          NULL);
 }
 
 void
@@ -100,27 +115,43 @@ teardown (void)
     if (expected_error)
         g_error_free(expected_error);
 
+    if (packet)
+        g_free(packet);
+
     if (tmp_dir) {
         cut_remove_path(tmp_dir, NULL);
         g_free(tmp_dir);
+    }
+
+    if (custom_config_path)
+        g_free(custom_config_path);
+}
+
+static gboolean
+cb_idle_check (gpointer data)
+{
+    gboolean *idle_emitted = data;
+
+    *idle_emitted = TRUE;
+    return FALSE;
+}
+
+static void
+pump_all_events (void)
+{
+    gboolean idle_emitted = FALSE;
+
+    g_idle_add(cb_idle_check, &idle_emitted);
+    while (!idle_emitted) {
+        g_main_context_iteration(NULL, TRUE);
     }
 }
 
 void
 test_set_configuration (void)
 {
-    gchar *packet;
-    gsize packet_size;
     const gchar configuration[] = "XXX";
-    gchar *custom_config_path;
     GError *error = NULL;
-
-    custom_config_path = g_build_filename(tmp_dir,
-                                          CUSTOM_CONFIG_FILE_NAME,
-                                          NULL);
-    cut_take_string(custom_config_path);
-
-    milter_manager_configuration_add_load_path(config, tmp_dir);
 
     cut_assert_path_not_exist(custom_config_path);
 
@@ -129,9 +160,8 @@ test_set_configuration (void)
         &packet, &packet_size,
         configuration, strlen(configuration));
     milter_writer_write(writer, packet, packet_size, NULL, &error);
-    g_free(packet);
     gcut_assert_error(error);
-    g_main_context_iteration(NULL, TRUE);
+    pump_all_events();
 
     cut_assert_path_exist(custom_config_path);
 
