045f418aa4a678b1671bf9456fb0bc6283bc4fd4
ktou <ktou@2eb25379-3f72-4156-9665-29aef8dc9b31>
2008-11-25 05:52:05 +0000

    * ui/www/milter-manager-admin/: add CRUD for config/milters/.

--- ui/www/milter-manager-admin/app/helpers/application_helper.rb
+++ ui/www/milter-manager-admin/app/helpers/application_helper.rb
@@ -1,3 +1,26 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def flash_box(message, options={})
+    return '' if message.nil?
+    id_suffix = options[:id_suffix] || ""
+    id = "flash-box#{id_suffix}"
+    fade_out = Proc.new {|page| page.visual_effect(:fade, id)}
+    message = content_tag(:p, h(message), :class => "notice")
+    flash_box_div = content_tag("div", "\n#{message}\n",
+                                :id => id,
+                                :class => "flash-box",
+                                :onclick => update_page(&fade_out))
+    if options[:need_container]
+      flash_box_div = content_tag("div", "\n  #{flash_box_div}\n",
+                                  :class => "flash-box-container")
+    end
+    set_opacity = update_page {|page| page[id].setOpacity("0.8")}
+    effect = update_page do |page|
+      page.delay(5) do
+        fade_out.call(page)
+      end
+    end
+    javascript_content = "#{set_opacity}\n#{effect}"
+    "#{flash_box_div}\n#{javascript_tag(javascript_content)}"
+  end
 end
