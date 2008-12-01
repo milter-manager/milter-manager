b0e7fa10c97c1033fc718cf251bf141009e75da2
ktou <ktou@2eb25379-3f72-4156-9665-29aef8dc9b31>
2008-12-01 08:01:43 +0000

    * binding/ruby/test/manager/test-custom-xml-loader.rb: add   a missing file.

---
+++ binding/ruby/test/manager/test-custom-xml-loader.rb
@@ -0,0 +1,28 @@
+class TestCustomXMLLoader < Test::Unit::TestCase
+  include MilterTestUtils
+
+  def setup
+    @configuration = Object.new
+    @loader = Milter::Manager::ConfigurationLoader.new(@configuration)
+  end
+
+  def test_security
+    mock(@configuration).privilege_mode=(true)
+    load(<<-EOC)
+<configuration>
+  <security>
+    <privilege-mode>true</privilege-mode>
+  </security>
+</configuration>
+EOC
+  end
+
+  private
+  def load(content)
+    file = Tempfile.new("xml-loader")
+    file.open
+    file.puts(content)
+    file.close
+    @loader.load_custom_configuration(file.path)
+  end
+end
