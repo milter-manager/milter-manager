c444e0242bacbd672486b319592b32729b5c36f5
ktou <ktou@2eb25379-3f72-4156-9665-29aef8dc9b31>
2008-12-08 06:40:10 +0000

    * binding/ruby/src/manager/rb-milter-manager-gstring.c: add   a missing file.

---
+++ binding/ruby/src/manager/rb-milter-manager-gstring.c
@@ -0,0 +1,67 @@
+/* -*- c-file-style: "ruby" -*- */
+
+#include "rb-milter-manager-private.h"
+
+#define SELF(self) (DATA_PTR(self))
+
+static VALUE rb_cGString;
+
+static void
+rb_gstring_free (GString *string)
+{
+    g_string_free(string, TRUE);
+}
+
+static VALUE
+rb_gstring_allocate (VALUE klass)
+{
+  return Data_Wrap_Struct(klass, NULL, rb_gstring_free, NULL);
+}
+
+static VALUE
+rb_gstring_initialize (VALUE self)
+{
+    SELF(self) = g_string_new(NULL);
+    return Qnil;
+}
+
+static VALUE
+rb_gstring_write (VALUE self, VALUE content)
+{
+    GString *string;
+
+    string = SELF(self);
+    g_string_append_len(string, RSTRING_PTR(content), RSTRING_LEN(content));
+    return LL2NUM(RSTRING_LEN(content));
+}
+
+static VALUE
+rb_gstring_to_s (VALUE self)
+{
+    GString *string;
+
+    string = SELF(self);
+    return rb_str_new(string->str, string->len);
+}
+
+VALUE
+rb_milter_manager_gstring_handle_to_xml_signal (guint num, const GValue *values)
+{
+    return rb_ary_new3(3,
+		       GVAL2RVAL(&values[0]),
+		       Data_Wrap_Struct(rb_cGString,
+					NULL,
+					NULL,
+					g_value_get_pointer(&values[1])),
+		       UINT2NUM(g_value_get_uint(&values[2])));
+}
+
+void
+Init_milter_manager_gstring (void)
+{
+    rb_cGString = rb_define_class_under(rb_mMilterManager, "GString", rb_cIO);
+    rb_define_alloc_func(rb_cGString, rb_gstring_allocate);
+    rb_define_method(rb_cGString, "initialize", rb_gstring_initialize, 0);
+    rb_define_method(rb_cGString, "write", rb_gstring_write, 1);
+    rb_define_method(rb_cGString, "to_s", rb_gstring_to_s, 0);
+}
