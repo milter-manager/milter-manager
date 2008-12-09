a0bbb1f2155f15df6306890f22af32ba328a1138
ikezoe <ikezoe@2eb25379-3f72-4156-9665-29aef8dc9b31>
2008-12-09 02:50:01 +0000

    * tool/milter-log-tool.rb: Added MilterGraphTimeSpan class.

--- tool/milter-log-tool.rb
+++ tool/milter-log-tool.rb
@@ -14,6 +14,57 @@ class MilterSession
   end
 end
 
+class MilterGraphTimeSpan
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+  end
+
+  def step
+    case @name
+    when "second"
+      1
+    when "minute"
+      60
+    when "hour"
+      3600 
+    end
+  end
+
+  def rows
+    case @name
+    when "second"
+      86400 # 1day
+    when "minute"
+      43200 # 4weeks
+    when "hour"
+      8760 # 365days
+    end
+  end
+
+  def default_start_time
+    case @name
+    when "second"
+      "-1h" # 1hour
+    when "minute"
+      "-12h" # 12hours
+    when "hour"
+      "-1d" # 1day
+    end
+  end
+
+  def adjust_time(time)
+    case @name
+    when "second"
+      time
+    when "minute"
+      Time.utc(0, time.min, time.hour, time.mday, time.mon, time.year, time.wday, time.yday, time.isdst, time.zone)
+    when "hour"
+      Time.utc(0, 0, time.hour, time.mday, time.mon, time.year, time.wday, time.yday, time.isdst, time.zone)
+    end
+  end
+end
+
 class MilterLogTool
   def initialize
     @log = nil
@@ -42,52 +93,8 @@ class MilterLogTool
     opts.parse!(argv)
   end
 
-  def rrd_name(target)
-    "#{@rrd_directory}/milter-log.#{target}.rrd"
-  end
-
-  def target_time_step(target)
-    case target
-    when "second"
-      1
-    when "minute"
-      60
-    when "hour"
-      3600 
-    end
-  end
-
-  def target_time_rows(target)
-    case target
-    when "second"
-      86400 # 1day
-    when "minute"
-      43200 # 4weeks
-    when "hour"
-      8760 # 365days
-    end
-  end
-
-  def target_default_start_time(target)
-    case target
-    when "second"
-      "-1h" # 1hour
-    when "minute"
-      "-12h" # 12hours
-    when "hour"
-      "-1d" # 1day
-    end
-  end
-
-  def adjust_time_for_target(time, target)
-    case target
-    when "second"
-      time
-    when "minute"
-      Time.utc(0, time.min, time.hour, time.mday, time.mon, time.year, time.wday, time.yday, time.isdst, time.zone)
-    when "hour"
-      Time.utc(0, 0, time.hour, time.mday, time.mon, time.year, time.wday, time.yday, time.isdst, time.zone)
-    end
+  def rrd_name(time_span)
+    "#{@rrd_directory}/milter-log.#{time_span.name}.rrd"
   end
 
   def collect_session(regex)
@@ -124,12 +131,12 @@ class MilterLogTool
       collect_session("milter-manager\\[.+\\]: \\[(.+)\\](Start|End).* session in (.*)\\((.+)\\)$")
   end
 
-  def count_sessions(sessions, target, last_update_time)
+  def count_sessions(sessions, time_span, last_update_time)
     counting = Hash::new
     sessions.each do |session|
       next if session.end_time == nil
-      start_time = adjust_time_for_target(session.start_time, target)
-      end_time = adjust_time_for_target(session.end_time, target)
+      start_time =time_span. adjust_time(session.start_time)
+      end_time = time_span.adjust_time(session.end_time)
 
       # ignore sessions which has been already registerd due to RRD.update fails on past time stamp
       if last_update_time
@@ -138,13 +145,13 @@ class MilterLogTool
       end
 
       # ignore recent sessions due to RRD.update fails on past time stamp
-      next if end_time >= adjust_time_for_target(@now, target)
-      next if start_time >= adjust_time_for_target(@now, target)
+      next if end_time >= time_span.adjust_time(@now)
+      next if start_time >= time_span.adjust_time(@now)
 
       start_time = start_time.to_i
       end_time = end_time.to_i
 
-      start_time.step(end_time, target_time_step(target)) do |time|
+      start_time.step(end_time, time_span.step) do |time|
         count = counting[time] ? counting[time] : 0
         count += 1
         counting[time] = count
@@ -153,16 +160,16 @@ class MilterLogTool
     counting
   end
 
-  def collect_data(target, last_update_time)
-    child_counting = count_sessions(@child_sessions, target, last_update_time)
-    client_counting = count_sessions(@client_sessions, target, last_update_time)
+  def collect_data(time_span, last_update_time)
+    child_counting = count_sessions(@child_sessions, time_span, last_update_time)
+    client_counting = count_sessions(@client_sessions, time_span, last_update_time)
     [child_counting, client_counting]
   end
 
-  def create_target_rrd(target, start_time)
-    step = target_time_step(target)
-    rows = target_time_rows(target)
-    RRD.create("#{rrd_name(target)}",
+  def create_time_span_rrd(time_span, start_time)
+    step = time_span.step
+    rows = time_span.rows
+    RRD.create("#{rrd_name(time_span)}",
         "--start", (start_time - 1).to_i.to_s,
         "--step", step,
         "DS:child_sessions:GAUGE:#{step}:0:U",
@@ -171,10 +178,10 @@ class MilterLogTool
         "RRA:AVERAGE:0.5:1:#{rows}")
   end
 
-  def update_db(target)
-    last_update_time = RRD.last("#{rrd_name(target)}") if File.exist?(rrd_name(target))
+  def update_db(time_span)
+    last_update_time = RRD.last("#{rrd_name(time_span)}") if File.exist?(rrd_name(time_span))
 
-    counting = collect_data(target, last_update_time)
+    counting = collect_data(time_span, last_update_time)
 
     if counting[0].length > 0
       end_time = counting[0].sort { |a, b| a[0] <=> b[0] }.last[0] + 1
@@ -185,13 +192,13 @@ class MilterLogTool
     end
 
     start_time = counting[0].sort { |a, b| a[0] <=> b[0] }.first[0]
-    create_target_rrd(target, start_time) unless File.exist?(rrd_name(target))
+    create_time_span_rrd(time_span, start_time) unless File.exist?(rrd_name(time_span))
 
-    start_time.to_i.step(end_time, target_time_step(target)) do |time|
+    start_time.to_i.step(end_time, time_span.step) do |time|
       child_count = counting[1][time] ? counting[1][time] : 0
       client_count = counting[0][time] ? counting[0][time] : 0
       p("update #{Time.at(time)}  #{child_count}:#{client_count}")
-      RRD.update("#{rrd_name(target)}",
+      RRD.update("#{rrd_name(time_span)}",
                  "#{time}:#{child_count}:#{client_count}")
     end
   end
@@ -200,20 +207,20 @@ class MilterLogTool
     @now = Time.now.utc
     collect_client_session
     collect_child_session
-    update_db("second")
-    update_db("minute")
-    update_db("hour")
+    update_db(MilterGraphTimeSpan.new("second"))
+    update_db(MilterGraphTimeSpan.new("minute"))
+    update_db(MilterGraphTimeSpan.new("hour"))
   end
 
-  def output_graph(target, start_time = nil, end_time = "now", width = 1000 , height = 250)
-    start_time = target_default_start_time(target) unless start_time
-    RRD.graph("#{@rrd_directory}/#{target}.png",
-              "--title", "per #{target}",
-              "DEF:client=#{rrd_name(target)}:client_sessions:MAX",
-              "DEF:child=#{rrd_name(target)}:child_sessions:MAX",
+  def output_graph(time_span, start_time = nil, end_time = "now", width = 1000 , height = 250)
+    start_time = time_span.default_start_time unless start_time
+    RRD.graph("#{@rrd_directory}/#{time_span.name}.png",
+              "--title", "per #{time_span.name}",
+              "DEF:client=#{rrd_name(time_span)}:client_sessions:MAX",
+              "DEF:child=#{rrd_name(time_span)}:child_sessions:MAX",
               "LINE1:child#00ff00:The number of milter",
               "LINE2:client#ff0000:The number of SMTP session",
-              "--step", target_time_step(target),
+              "--step", time_span.step,
               "--start", start_time,
               "--end", end_time,
               "--width","#{width}",
@@ -221,9 +228,9 @@ class MilterLogTool
   end
 
   def output_all_graph
-    output_graph("second")
-    output_graph("minute")
-    output_graph("hour")
+    output_graph(MilterGraphTimeSpan.new("second"))
+    output_graph(MilterGraphTimeSpan.new("minute"))
+    output_graph(MilterGraphTimeSpan.new("hour"))
   end
 end
 
