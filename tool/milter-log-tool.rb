fe768d2829890875b213698637f9071c48024a2d
ikezoe <ikezoe@2eb25379-3f72-4156-9665-29aef8dc9b31>
2008-12-11 04:45:13 +0000

    * tool/milter-log-tool.rb: Added MilterPassChildRDD class.

--- tool/milter-log-tool.rb
+++ tool/milter-log-tool.rb
@@ -14,15 +14,6 @@ class MilterSession
   end
 end
 
-class MilterChildPassData
-  attr_accessor :name, :state, :time
-  def initialize(name, state, time)
-    @name = name
-    @state = state
-    @time = time
-  end
-end
-
 class MilterRRDCount < Hash
   def [](key)
     super(key) ? super(key) : store(key, Hash.new("U"))
@@ -127,7 +118,7 @@ class MilterRRD
     @rrd_directory = rrd_directory
     @log = log
     @update_time = update_time
-    @data = nil
+    @data = []
   end
 
   def count(time_span, last_update_time)
@@ -167,6 +158,9 @@ class MilterRRD
   end
 end
 
+class MilterSessionRRD < MilterRRD
+end
+
 class MilterMailStatusRRD < MilterRRD
   class MilterMail
     attr_accessor :status, :time, :key
@@ -182,7 +176,6 @@ class MilterMailStatusRRD < MilterRRD
   end
 
   def collect
-    @data = []
     @log.each do |line|
       if /milter-manager\[.+\]: \[(.+)\]Reply (.+) to MTA on (.+)$/ =~ line
         time = Time.parse($1)
@@ -266,14 +259,125 @@ class MilterMailStatusRRD < MilterRRD
   end
 end
 
+class MilterPassChildRRD < MilterRRD
+  class MilterPassChildData
+    attr_accessor :name, :state, :time, :key
+    def initialize(name, state, time)
+      @name = name
+      @state = state
+      @time = time
+      @key = state
+    end
+  end
+
+  def rrd_name(time_span)
+    "#{@rrd_directory}/milter-log.state.#{time_span.name}.rrd"
+  end
+
+  def collect
+    @log.each do |line|
+      if /milter-manager\[.+\]: \[(.+)\]Pass filter process of (.+)\(.+\) on (.+)$/ =~ line
+        time = Time.parse($1)
+        name = $2
+        state = $3
+        @data << MilterPassChildData.new(name, state, time)
+      end
+    end
+  end
+
+  def collect_data(time_span, last_update_time)
+    pass_counting = count(time_span, last_update_time)
+#    pass_counting["all"] = count_sessions(@child_sessions, time_span, last_update_time)
+    MilterRRDData.new(pass_counting)
+  end
+
+  def create_rrd(time_span, start_time)
+    step = time_span.step
+    rows = time_span.rows
+    RRD.create("#{rrd_name(time_span)}",
+        "--start", (start_time - 1).to_i.to_s,
+        "--step", step,
+        "DS:all:GAUGE:#{step}:0:U",
+        "DS:connect:GAUGE:#{step}:0:U",
+        "DS:helo:GAUGE:#{step}:0:U",
+        "DS:envelope-from:GAUGE:#{step}:0:U",
+        "DS:envelope-recipient:GAUGE:#{step}:0:U",
+        "DS:header:GAUGE:#{step}:0:U",
+        "DS:body:GAUGE:#{step}:0:U",
+        "DS:end-of-message:GAUGE:#{step}:0:U",
+        "RRA:MAX:0.5:1:#{rows}",
+        "RRA:AVERAGE:0.5:1:#{rows}")
+  end
+
+  def update_db(time_span)
+    last_update_time = RRD.last("#{rrd_name(time_span)}") if File.exist?(rrd_name(time_span))
+
+    data = collect_data(time_span, last_update_time)
+    return if data.empty?
+
+    end_time = data.last_time
+    start_time = last_update_time ? last_update_time + time_span.step : data.first_time
+
+    create_state_distinct_rrd(time_span, start_time) unless File.exist?(rrd_name(time_span))
+    start_time.to_i.step(end_time, time_span.step) do |time|
+      RRD.update("#{rrd_name(time_span)}",
+                 "#{time}" +
+                 ":#{data["all"][time]}" +
+                 ":#{data["connect"][time]}:#{data["helo"][time]}" +
+                 ":#{data["envelope-from"][time]}:#{data["envelope-recipient"][time]}" +
+                 ":#{data["header"][time]}:#{data["body"][time]}" +
+                 ":#{data["end-of-message"][time]}")
+    end
+  end
+
+  def output_graph(time_span, start_time = nil, end_time = "now", width = 1000, height = 250)
+    start_time = time_span.default_start_time unless start_time
+    rrd_file = rrd_name(time_span)
+    return unless File.exist?(rrd_file)
+    RRD.graph("#{@rrd_directory}/pass-child.#{time_span.name}.png",
+              "--title", "Processed mails per #{time_span.name}",
+              "DEF:all=#{rrd_file}:all:MAX",
+              "DEF:connect=#{rrd_file}:connect:MAX",
+              "DEF:helo=#{rrd_file}:helo:MAX",
+              "DEF:envelope-from=#{rrd_file}:envelope-from:MAX",
+              "DEF:envelope-recipient=#{rrd_file}:envelope-recipient:MAX",
+              "DEF:header=#{rrd_file}:header:MAX",
+              "DEF:body=#{rrd_file}:body:MAX",
+              "DEF:end-of-message=#{rrd_file}:end-of-message:MAX",
+              "CDEF:n_all=all,UN,0,all,IF",
+              "CDEF:n_connect=connect,UN,0,connect,IF",
+              "CDEF:n_helo=helo,UN,0,helo,IF",
+              "CDEF:n_envelope-from=envelope-from,UN,0,envelope-from,IF",
+              "CDEF:n_envelope-recipient=envelope-recipient,UN,0,envelope-recipient,IF",
+              "CDEF:n_header=header,UN,0,header,IF",
+              "CDEF:n_body=body,UN,0,body,IF",
+              "CDEF:n_end-of-message=end-of-message,UN,0,end-of-message,IF",
+              "AREA:n_all#0000ff:The number of milters",
+              "AREA:n_connect#ff0000:Passed on connect ",
+              "STACK:n_helo#dd2200:Passed on helo ",
+              "STACK:n_envelope-from#bb4400:Passed on envelope-from ",
+              "STACK:n_envelope-recipient#996600:Passed on envelope-recipient ",
+              "STACK:n_header#778800:Passed on header ",
+              "STACK:n_body#55aa00:Passed on body ",
+              "STACK:n_end-of-message#33cc00:Passed on end-of-message ",
+              "--step", time_span.step,
+              "--start", start_time,
+              "--end", end_time,
+              "--width","#{width}",
+              "--height", "#{height}",
+              "--alt-y-grid",
+              "COMMENT:Last update\\: #{@update_time.localtime.rfc2822.gsub!(/:/,'\\:')}\\r")
+  end
+end
+
 class MilterLogTool
   def initialize
     @log = nil
     @child_sessions = nil
     @client_sessions = nil
-    @pass_filters = nil
     @update_db = false
     @mail_status = nil
+    @pass_child = nil
   end
 
   def parse_options(argv)
@@ -297,10 +401,6 @@ class MilterLogTool
 
   def rrd_name(time_span)
     "#{@rrd_directory}/milter-log.#{time_span.name}.rrd"
-  end
-
-  def state_rrd_name(time_span)
-    "#{@rrd_directory}/milter-log.state.#{time_span.name}.rrd"
   end
 
   def collect_session(regex)
@@ -327,19 +427,6 @@ class MilterLogTool
     sessions
   end
 
-  def collect_pass_filters(regex)
-    filters = []
-    @log.each do |line|
-      if Regexp.new(regex).match(line)
-        time = Time.parse($1)
-        name = $2
-        state = $3
-        filters << MilterChildPassData.new(name, state, time)
-      end
-    end
-    filters
-  end
-
   def collect_child_session
     @child_sessions = 
       collect_session("milter-manager\\[.+\\]: \\[(.+)\\](Start|End).* filter process of (.*)\\((.+)\\)$")
@@ -348,11 +435,6 @@ class MilterLogTool
   def collect_client_session
     @client_sessions = 
       collect_session("milter-manager\\[.+\\]: \\[(.+)\\](Start|End).* session in (.*)\\((.+)\\)$")
-  end
-
-  def collect_pass_filter
-    @pass_filters =
-      collect_pass_filters("milter-manager\\[.+\\]: \\[(.+)\\]Pass filter process of (.+)\\(.+\\) on (.+)$")
   end
 
   def count_sessions(sessions, time_span, last_update_time)
@@ -385,59 +467,11 @@ class MilterLogTool
     counting
   end
 
-  def count_pass_filters(pass_filters, time_span, last_update_time)
-    counting = MilterRRDCount.new()
-    pass_filters.each do |pass_filter|
-      time =time_span.adjust_time(pass_filter.time)
-
-      # ignore sessions which has been already registerd due to RRD.update fails on past time stamp
-      if last_update_time
-        next if time <= last_update_time
-      end
-
-      # ignore recent sessions due to RRD.update fails on past time stamp
-      next if time >= time_span.adjust_time(@now)
-
-      time = time.to_i
-      state = pass_filter.state
-
-      count = counting[state][time]
-      count = 0 if count == "U"
-      count += 1
-      counting[state][time] = count
-    end
-    counting
-  end
-
   def collect_session_data(time_span, last_update_time)
     counting = MilterRRDCount.new()
     counting["child"] = count_sessions(@child_sessions, time_span, last_update_time)
     counting["session"] = count_sessions(@client_sessions, time_span, last_update_time)
     MilterRRDData.new(counting)
-  end
-
-  def collect_pass_filter_data(time_span, last_update_time)
-    pass_counting = count_pass_filters(@pass_filters, time_span, last_update_time)
-    pass_counting["all"] = count_sessions(@child_sessions, time_span, last_update_time)
-    MilterRRDData.new(pass_counting)
-  end
-
-  def create_state_distinct_rrd(time_span, start_time)
-    step = time_span.step
-    rows = time_span.rows
-    RRD.create("#{state_rrd_name(time_span)}",
-        "--start", (start_time - 1).to_i.to_s,
-        "--step", step,
-        "DS:all:GAUGE:#{step}:0:U",
-        "DS:connect:GAUGE:#{step}:0:U",
-        "DS:helo:GAUGE:#{step}:0:U",
-        "DS:envelope-from:GAUGE:#{step}:0:U",
-        "DS:envelope-recipient:GAUGE:#{step}:0:U",
-        "DS:header:GAUGE:#{step}:0:U",
-        "DS:body:GAUGE:#{step}:0:U",
-        "DS:end-of-message:GAUGE:#{step}:0:U",
-        "RRA:MAX:0.5:1:#{rows}",
-        "RRA:AVERAGE:0.5:1:#{rows}")
   end
 
   def create_session_rrd(time_span, start_time)
@@ -450,27 +484,6 @@ class MilterLogTool
         "DS:child_sessions:GAUGE:#{step}:0:U",
         "RRA:MAX:0.5:1:#{rows}",
         "RRA:AVERAGE:0.5:1:#{rows}")
-  end
-
-  def update_state_db(time_span)
-    last_update_time = RRD.last("#{state_rrd_name(time_span)}") if File.exist?(state_rrd_name(time_span))
-
-    data = collect_pass_filter_data(time_span, last_update_time)
-    return if data.empty?
-
-    end_time = data.last_time
-    start_time = last_update_time ? last_update_time + time_span.step : data.first_time
-
-    create_state_distinct_rrd(time_span, start_time) unless File.exist?(state_rrd_name(time_span))
-    start_time.to_i.step(end_time, time_span.step) do |time|
-      RRD.update("#{state_rrd_name(time_span)}",
-                 "#{time}" +
-                 ":#{data["all"][time]}" +
-                 ":#{data["connect"][time]}:#{data["helo"][time]}" +
-                 ":#{data["envelope-from"][time]}:#{data["envelope-recipient"][time]}" +
-                 ":#{data["header"][time]}:#{data["body"][time]}" +
-                 ":#{data["end-of-message"][time]}")
-    end
   end
 
   def update_db(time_span)
@@ -505,18 +518,16 @@ class MilterLogTool
 
     collect_client_session
     collect_child_session
-    collect_pass_filter
 
     @mail_status = MilterMailStatusRRD.new(@rrd_directory, @log, @now)
     @mail_status.update
     
+    @pass_child = MilterPassChildRRD.new(@rrd_directory, @log, @now)
+    @pass_child.update
+    
     update_db(MilterGraphTimeSpan.new("second"))
     update_db(MilterGraphTimeSpan.new("minute"))
     update_db(MilterGraphTimeSpan.new("hour"))
-
-    update_state_db(MilterGraphTimeSpan.new("second"))
-    update_state_db(MilterGraphTimeSpan.new("minute"))
-    update_state_db(MilterGraphTimeSpan.new("hour"))
   end
 
   def output_session_graph(time_span, start_time = nil, end_time = "now", width = 1000 , height = 250)
@@ -540,49 +551,10 @@ class MilterLogTool
               "COMMENT:Last update\\: #{@now.localtime.rfc2822.gsub!(/:/,'\\:')}\\r")
   end
 
-  def output_pass_filter_graph(time_span, start_time = nil, end_time = "now", width = 1000, height = 250)
-    start_time = time_span.default_start_time unless start_time
-    rrd_file = state_rrd_name(time_span)
-    return unless File.exist?(rrd_file)
-    RRD.graph("#{@rrd_directory}/pass-filter.#{time_span.name}.png",
-              "--title", "Processed mails per #{time_span.name}",
-              "DEF:all=#{rrd_file}:all:MAX",
-              "DEF:connect=#{rrd_file}:connect:MAX",
-              "DEF:helo=#{rrd_file}:helo:MAX",
-              "DEF:envelope-from=#{rrd_file}:envelope-from:MAX",
-              "DEF:envelope-recipient=#{rrd_file}:envelope-recipient:MAX",
-              "DEF:header=#{rrd_file}:header:MAX",
-              "DEF:body=#{rrd_file}:body:MAX",
-              "DEF:end-of-message=#{rrd_file}:end-of-message:MAX",
-              "CDEF:n_all=all,UN,0,all,IF",
-              "CDEF:n_connect=connect,UN,0,connect,IF",
-              "CDEF:n_helo=helo,UN,0,helo,IF",
-              "CDEF:n_envelope-from=envelope-from,UN,0,envelope-from,IF",
-              "CDEF:n_envelope-recipient=envelope-recipient,UN,0,envelope-recipient,IF",
-              "CDEF:n_header=header,UN,0,header,IF",
-              "CDEF:n_body=body,UN,0,body,IF",
-              "CDEF:n_end-of-message=end-of-message,UN,0,end-of-message,IF",
-              "AREA:n_all#0000ff:The number of milters",
-              "AREA:n_connect#ff0000:Passed on connect ",
-              "STACK:n_helo#dd2200:Passed on helo ",
-              "STACK:n_envelope-from#bb4400:Passed on envelope-from ",
-              "STACK:n_envelope-recipient#996600:Passed on envelope-recipient ",
-              "STACK:n_header#778800:Passed on header ",
-              "STACK:n_body#55aa00:Passed on body ",
-              "STACK:n_end-of-message#33cc00:Passed on end-of-message ",
-              "--step", time_span.step,
-              "--start", start_time,
-              "--end", end_time,
-              "--width","#{width}",
-              "--height", "#{height}",
-              "--alt-y-grid",
-              "COMMENT:Last update\\: #{@now.localtime.rfc2822.gsub!(/:/,'\\:')}\\r")
-  end
-
   def output_graph(time_span)
     output_session_graph(MilterGraphTimeSpan.new(time_span))
-    output_pass_filter_graph(MilterGraphTimeSpan.new(time_span))
     @mail_status.output_graph(MilterGraphTimeSpan.new(time_span))
+    @pass_child.output_graph(MilterGraphTimeSpan.new(time_span))
   end
 
   def output_all_graph
