9a3e057f2ee496adffc0fb87683bd05bc036f8e8
ktou <ktou@2eb25379-3f72-4156-9665-29aef8dc9b31>
2008-12-17 08:49:25 +0000

    * doc/reference/, milter/client.h: add documentation for   milter/client.h.

--- milter/client.h
+++ milter/client.h
@@ -20,22 +20,131 @@
 #ifndef __MILTER_CLIENT_H__
 #define __MILTER_CLIENT_H__
 
+#include <milter/core.h>
 #include <milter/client/milter-client-context.h>
 #include <milter/client/milter-client-enum-types.h>
-#include <milter/core/milter-error-emittable.h>
 
 G_BEGIN_DECLS
+
+/**
+ * SECTION: client
+ * @title: Client library
+ * @short_description: The client library to implement a milter.
+ *
+ * The milter-client library provides full milter protocol
+ * processing features on client-side. "client-side" means
+ * milter-side not MTA-side.
+ *
+ * %MilterClient and %MilterClientContext is the main
+ * classes in the milter-client library. %MilterClient
+ * accepts n-connections from MTA and each connection is
+ * processed by %MilterClientContext.
+ *
+ * </para><refsect2><title>Usage overview</title><para>
+ * You need to set connection spec and connect
+ * #MilterClient::connection-established signal before
+ * entering mail loop.
+ *
+ * Connection spec is a entry point of %MilterClient for
+ * MTA.  It has 'PROTOCOL:INFORMATION' format. For IPv4
+ * socket, 'inet:PORT' or 'inet:PORT@HOST' are valid
+ * formats. For IPv6 socket, 'inet6:PORT' or
+ * 'inet6:PORT@HOST' are valid formats. For UNIX domain
+ * socket, 'unix:PATH' is a valid format.
+ *
+ * %MilterClient emits #MilterClient::connection-established
+ * signal. %MilterClientContext setup should be done in the
+ * signal. In many cases, you just connect callbacks to
+ * the passed %MilterClientContext. See %MilterClientContext
+ * for available signals.
+ *
+ * milter_client_main() is mail loop function. You can enter
+ * into mail loop after you finish to prepare your
+ * %MilterClient.
+ *
+ * Here is an example codes that uses the milter-client
+ * library. See also tool/milter-test-client.c. It is an
+ * example milter implementation using the milter-client
+ * library.
+ *
+ * |[
+ * #include <stdlib.h>
+ * #include <milter/client.h>
+ *
+ * static void
+ * cb_connection_established (MilterClient *client,
+ *                            MilterClientContext *context,
+ *                            gpointer user_data)
+ * {
+ *    connect_to_your_interesting_signals(client, context, user_data);
+ * }
+ *
+ * int
+ * main (int argc, char **argv)
+ * {
+ *     gboolean success;
+ *     const gchar spec[] = "inet:10025@localhost";
+ *     MilterClient *client;
+ *     GError *error = NULL;
+ *
+ *     milter_init();
+ *
+ *     client = milter_client_new();
+ *     if (!milter_client_set_connection_spec(client, spec, &error)) {
+ *         g_print("%s\n", error->message);
+ *         g_error_free(error);
+ *         return EXIT_FAILURE;
+ *     }
+ *     g_signal_connect(client, "connection-established",
+ *                      G_CALLBACK(cb_connection_established), NULL);
+ *     milter_client_main(client);
+ *
+ *     milter_quit();
+ *
+ *     return success ? EXIT_SUCCESS : EXIT_FAILURE;
+ * }
+ * ]|
+ * </para></refsect2><para>
+ *
+ * </para><refsect2><title>Processing model</title><para>
+ * The libmilter provided by Sendmail uses a thread for each
+ * connection model. But the milter-client library doesn't
+ * use it. The milter-client library uses two threads. One
+ * thread is for %MilterClient and other thread is for
+ * %MilterClientContext<!-- -->s. %MilterClient just
+ * accepting connections from MTA and dispatches it in the
+ * first thread. Each connection is associated with
+ * %MilterClientContext and they are processed in the second
+ * thread. The milter-client library's model is the same
+ * model of memcached. (memcached uses libevent as its event
+ * loop backend but the milter-client library uses GLib's
+ * %GMainLoop for it.)
+ *
+ * FIXME (should be confirmed): The libmilter's model will
+ * have more cost to accepts rather than the milter-client
+ * library's model. Because the libmilter's model creates a
+ * thread but the milter-client library just allocate
+ * %MilterClientContext.
+ * </para></refsect2><para>
+ */
+
 
 #define MILTER_TYPE_CLIENT            (milter_client_get_type())
 #define MILTER_CLIENT(obj)            (G_TYPE_CHECK_INSTANCE_CAST((obj), MILTER_TYPE_CLIENT, MilterClient))
 #define MILTER_CLIENT_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST((klass), MILTER_TYPE_CLIENT, MilterClientClass))
-#define MILTER_CLIENT_IS(obj)         (G_TYPE_CHECK_INSTANCE_TYPE((obj), MILTER_TYPE_CLIENT))
-#define MILTER_CLIENT_IS_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE((klass), MILTER_TYPE_CLIENT))
+#define MILTER_IS_CLIENT(obj)         (G_TYPE_CHECK_INSTANCE_TYPE((obj), MILTER_TYPE_CLIENT))
+#define MILTER_IS_CLIENT_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE((klass), MILTER_TYPE_CLIENT))
 #define MILTER_CLIENT_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS((obj), MILTER_TYPE_CLIENT, MilterClientClass))
 
 typedef struct _MilterClient         MilterClient;
 typedef struct _MilterClientClass    MilterClientClass;
 
+/**
+ * MilterClient:
+ *
+ * %MilterClient is a front object. It accepts connections
+ * from MTA and dispatches it.
+ **/
 struct _MilterClient
 {
     GObject object;
