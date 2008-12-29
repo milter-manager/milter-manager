98ede70a31f4aa9dab393e05a2a0feb338eea31d
ktou <ktou@2eb25379-3f72-4156-9665-29aef8dc9b31>
2008-12-29 13:48:38 +0000

    * test/client/test-client-context-modify.c (test_add_header): add   a written packet assertion.

--- test/client/test-client-context-modify.c
+++ test/client/test-client-context-modify.c
@@ -32,10 +32,20 @@ void test_add_header (gconstpointer _data);
 
 static MilterClientContext *context;
 
+static GIOChannel *channel;
+
 void
 setup (void)
 {
+    MilterWriter *writer;
+
     context = milter_client_context_new();
+
+    channel = gcut_string_io_channel_new(NULL);
+    g_io_channel_set_encoding(channel, NULL, NULL);
+    writer = milter_writer_io_channel_new(channel);
+    milter_agent_set_writer(MILTER_AGENT(context), writer);
+    g_object_unref(writer);
 }
 
 void
@@ -43,19 +53,36 @@ teardown (void)
 {
     if (context)
         g_object_unref(context);
+
+    if (channel)
+        g_io_channel_unref(channel);
 }
 
 void
 data_add_header (void)
 {
+    MilterReplyEncoder *reply_encoder;
+
+    reply_encoder = MILTER_REPLY_ENCODER(milter_reply_encoder_new());
+
 #define ADD(label, name, value, state, action_names, error) do          \
     {                                                                   \
         MilterActionFlags action;                                       \
         GError *parse_error = NULL;                                     \
+        gchar *packet = NULL;                                           \
+        gsize packet_size = 0;                                          \
                                                                         \
         action = gcut_flags_parse(MILTER_TYPE_ACTION_FLAGS,             \
                                   action_names, &parse_error);          \
         gcut_assert_error(parse_error);                                 \
+                                                                        \
+        if (!error) {                                                   \
+            milter_reply_encoder_encode_add_header(reply_encoder,       \
+                                                   &packet,             \
+                                                   &packet_size,        \
+                                                   name, value);        \
+        }                                                               \
+                                                                        \
         gcut_add_datum(label,                                           \
                        "name", G_TYPE_STRING, name,                     \
                        "value", G_TYPE_STRING, value,                   \
@@ -63,7 +90,12 @@ data_add_header (void)
                        MILTER_CLIENT_CONTEXT_STATE_ ## state,           \
                        "action", MILTER_TYPE_ACTION_FLAGS, action,      \
                        "error", G_TYPE_POINTER, error, g_error_free,    \
+                       "packet", G_TYPE_POINTER,                        \
+                       g_memdup(packet, packet_size), g_free,           \
+                       "packet-size", G_TYPE_UINT, packet_size,         \
                        NULL);                                           \
+        if (packet)                                                     \
+            g_free(packet);                                             \
     } while (0)
 
     ADD("success",
@@ -93,6 +125,8 @@ data_add_header (void)
                     "add-header can only be done with "
                     "<add-headers> action flags: <>"));
 #undef ADD
+
+    g_object_unref(reply_encoder);
 }
 
 void
@@ -101,6 +135,7 @@ test_add_header (gconstpointer _data)
     const GCutData *data = _data;
     MilterOption *option;
     const GError *expected_error;
+    gboolean success;
     GError *actual_error = NULL;
 
     milter_client_context_set_state(context,
@@ -110,16 +145,28 @@ test_add_header (gconstpointer _data)
                                0);
     milter_client_context_set_option(context, option);
     g_object_unref(option);
-    milter_client_context_add_header(context,
-                                     gcut_data_get_string(data, "name"),
-                                     gcut_data_get_string(data, "value"),
-                                     &actual_error);
+
+    success =
+        milter_client_context_add_header(context,
+                                         gcut_data_get_string(data, "name"),
+                                         gcut_data_get_string(data, "value"),
+                                         &actual_error);
     expected_error = gcut_data_get_pointer(data, "error");
     if (expected_error) {
         gcut_take_error(actual_error);
         gcut_assert_equal_error(expected_error, actual_error);
+        cut_assert_false(success);
     } else {
+        GString *actual_data;
+
         gcut_assert_error(actual_error);
+
+        actual_data = gcut_string_io_channel_get_string(channel);
+        cut_assert_equal_memory(gcut_data_get_pointer(data, "packet"),
+                                gcut_data_get_uint(data, "packet-size"),
+                                actual_data->str,
+                                actual_data->len);
+        cut_assert_true(success);
     }
 }
 
