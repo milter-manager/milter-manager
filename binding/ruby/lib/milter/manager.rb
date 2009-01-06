ae28895d50b623a8797f6ee66ec7d086baafac87
ktou <ktou@2eb25379-3f72-4156-9665-29aef8dc9b31>
2009-01-06 08:47:05 +0000

    * binding/ruby/lib/milter/manager.rb,   binding/ruby/test/manager/test-debian-init-detector.rb,   data/init.d/milter-manager: add Debian's init script detector.

--- binding/ruby/lib/milter/manager.rb
+++ binding/ruby/lib/milter/manager.rb
@@ -829,5 +829,159 @@ class ConfigurationLoader
         "/etc/rc.conf.d"
       end
     end
+
+    class DebianInitDetector
+      attr_reader :name, :variables, :info
+      def initialize(script_name, &connection_spec_detector)
+        @script_name = script_name
+        @connection_spec_detector = connection_spec_detector
+        init_variables
+      end
+
+      def detect
+        init_variables
+        return unless init_script_exist?
+
+        parse_init_script
+        return if @name.nil?
+        parse_default_conf(default_conf)
+      end
+
+      def init_script_exist?
+        File.exist?(init_script)
+      end
+
+      def description
+        @info["Description"] || @variables["DESC"]
+      end
+
+      def enabled?
+        @variables["ENABLED"] != "0"
+      end
+
+      def apply(loader)
+        return if @name.nil?
+        connection_spec = guess_connection_spec
+        return if connection_spec.nil?
+        connection_spec = "unix:#{connection_spec}" if /\A\// =~ connection_spec
+        loader.define_milter(@name) do |milter|
+          milter.enabled = enabled?
+          milter.description = description
+          milter.command = init_script
+          milter.command_options = "start"
+          milter.connection_spec = connection_spec
+          yield(milter) if block_given?
+        end
+      end
+
+      private
+      def init_variables
+        @name = nil
+        @info = {}
+        @variables = {}
+      end
+
+      VARIABLE_DEFINITION_RE = /\b([A-Z\d_]+)=(".*?"|\S*)/
+      def parse_init_script
+        content = File.open(init_script).read
+        info = content.scan(/^### BEGIN INIT INFO\n(.+)\n### END INIT INFO$/m)
+        parse_init_info(info[0][0]) if info[0]
+        extract_variables(content)
+
+        @name = @variables["NAME"] || @info["Provides"] || @script_name
+      end
+
+      def parse_init_info(info)
+        info = info.gsub(/^# /, '').split(/([a-zA-Z\-]+):\s*/m)[1..-1]
+        0.step(info.size - 1, 2) do |i|
+          key, value = info[i], (info[i + 1] || '').strip
+          @info[key] = value
+        end
+      end
+
+      def parse_default_conf(file)
+        return unless File.exist?(file)
+        File.open(file) do |input|
+          extract_variables(input)
+        end
+      end
+
+      def extract_variables(input)
+        input.each_line do |line|
+          case line
+          when VARIABLE_DEFINITION_RE
+            variable_name, variable_value = $1, $2
+            variable_value = normalize_variable_value(variable_value)
+            @variables[variable_name] = variable_value
+          end
+        end
+      end
+
+      def normalize_variable_value(value)
+        value = value.sub(/\A"(.*)"\z/, '\\1')
+        variable_expand_re = /\$(\{?)([a-zA-Z\d_]+)(\}?)/
+        value = value.gsub(variable_expand_re) do |matched|
+          left_brace, name, right_brace = $1, $2, $3
+          if [left_brace, right_brace] == ["{", "}"] or
+              [left_brace, right_brace] == ["", ""]
+            @variables[name] || matched
+          else
+            matched
+          end
+        end
+      end
+
+      def guess_connection_spec
+        spec = nil
+        if @connection_spec_detector
+          spec = @connection_spec_detector.call(self)
+        end
+        spec ||= @variables["SOCKET"] || @variables["SOCKFILE"]
+        spec ||= @variables["CONNECTION_SPEC"]
+        spec ||= extract_connection_spec_parameter_from_flags(@variables["OPTARGS"])
+        spec ||= extract_connection_spec_parameter_from_flags(@variables["DAEMON_ARGS"])
+        spec ||= extract_connection_spec_parameter_from_flags(@variables["PARAMS"])
+        spec
+      end
+
+      def extract_connection_spec_parameter_from_flags(flags)
+        return nil if flags.nil?
+
+        options = Shellwords.split(flags)
+        p_option_index = options.index("-p")
+        if p_option_index
+          spec = options[p_option_index + 1]
+          return spec unless shell_variable?(spec)
+        else
+          options.each do |option|
+            if /\A-p/ =~ option
+              spec = $POSTMATCH
+              return spec unless shell_variable?(spec)
+            end
+          end
+        end
+        nil
+      end
+
+      def shell_variable?(string)
+        /\A\$/ =~ string
+      end
+
+      def init_script
+        File.join(init_d, @script_name)
+      end
+
+      def default_conf
+        File.join(default_dir, @name)
+      end
+
+      def init_d
+        "/etc/init.d"
+      end
+
+      def default_dir
+        "/etc/default"
+      end
+    end
   end
 end
---
+++ binding/ruby/test/manager/test-debian-init-detector.rb
@@ -0,0 +1,401 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2008  Kouhei Sutou <kou@cozmixng.org>
+#
+# This library is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
+class TestDebianInitDetector < Test::Unit::TestCase
+  include MilterTestUtils
+
+  def setup
+    @configuration = Milter::Manager::Configuration.new
+    @loader = Milter::Manager::ConfigurationLoader.new(@configuration)
+    @tmp_dir = Pathname(File.dirname(__FILE__)) + ".." + "tmp"
+    @init_d = @tmp_dir + "init.d"
+    @default_dir = @tmp_dir + "default"
+    @init_d.mkpath
+    @default_dir.mkpath
+  end
+
+  def teardown
+    FileUtils.rm_rf(@tmp_dir.to_s)
+  end
+
+  def test_init_script_exist?
+    detector = debain_init_detector("milter-manager")
+    assert_false(detector.init_script_exist?)
+    (@init_d + "milter-manager").open("w") {}
+    assert_true(detector.init_script_exist?)
+  end
+
+  def test_name
+    (@init_d + "milter-manager").open("w") do |file|
+      file << milter_manager_init_script
+    end
+    detector = debain_init_detector("milter-manager")
+    detector.detect
+    assert_equal("milter-manager", detector.name)
+  end
+
+  def test_variables
+    (@init_d + "milter-manager").open("w") do |file|
+      file << milter_manager_init_script
+    end
+    detector = debain_init_detector("milter-manager")
+    detector.detect
+    assert_equal({
+                   "PATH" => "/sbin:/usr/sbin:/bin:/usr/bin",
+                   "DESC" => "flexible milter management daemon",
+                   "NAME" => "milter-manager",
+                   "DAEMON" => "/usr/sbin/milter-manager",
+                   "USER" => "milter-manager",
+                   "GROUP" => "mail",
+                   "PIDFILE" => "/var/run/milter-manager/milter-manager.pid",
+                   "CONNECTION_SPEC" => "unix:/var/run/milter-manager/milter-manager.sock",
+                   "SCRIPTNAME" => "/etc/init.d/milter-manager",
+                   "DAEMON_ARGS" => ["--daemon",
+                                     "--pid-file",
+                                     "/var/run/milter-manager/milter-manager.pid",
+                                     "--connection-spec",
+                                     "unix:/var/run/milter-manager/milter-manager.sock",
+                                     "--user-name",
+                                     "milter-manager",
+                                     "--group-name",
+                                     "mail"].join(" ") + " ",
+                   "OPTION_ARGS" => "",
+                   "RETVAL" => "$?",
+                 },
+                 detector.variables)
+  end
+
+  def test_info
+    (@init_d + "milter-manager").open("w") do |file|
+      file << milter_manager_init_script
+    end
+    detector = debain_init_detector("milter-manager")
+    detector.detect
+    assert_equal({
+                   "Provides" => "milter-manager",
+                   "Default-Start" => "2 3 4 5",
+                   "Default-Stop" => "0 1 6",
+                   "Short-Description" => "milter-manager's init script",
+                   "Description" =>
+                     "The milter that manages milters to combine them flexibly.",
+                 },
+                 detector.info)
+  end
+
+  def test_parse_init_conf
+    (@init_d + "milter-manager").open("w") do |file|
+      file << milter_manager_init_script
+    end
+    (@default_dir + "milter-manager").open("w") do |file|
+      file << <<-EOC
+ENABLED="0"
+CONNECTION_SPEC=inet:10025@localhost
+EOC
+    end
+
+    detector = debain_init_detector("milter-manager")
+    detector.detect
+    assert_equal({
+                   "ENABLED" => "0",
+                   "PATH" => "/sbin:/usr/sbin:/bin:/usr/bin",
+                   "DESC" => "flexible milter management daemon",
+                   "NAME" => "milter-manager",
+                   "DAEMON" => "/usr/sbin/milter-manager",
+                   "USER" => "milter-manager",
+                   "GROUP" => "mail",
+                   "PIDFILE" => "/var/run/milter-manager/milter-manager.pid",
+                   "CONNECTION_SPEC" => "inet:10025@localhost",
+                   "SCRIPTNAME" => "/etc/init.d/milter-manager",
+                   "DAEMON_ARGS" => ["--daemon",
+                                     "--pid-file",
+                                     "/var/run/milter-manager/milter-manager.pid",
+                                     "--connection-spec",
+                                     "unix:/var/run/milter-manager/milter-manager.sock",
+                                     "--user-name",
+                                     "milter-manager",
+                                     "--group-name",
+                                     "mail"].join(" ") + " ",
+                   "OPTION_ARGS" => "",
+                   "RETVAL" => "$?",
+                 },
+                 detector.variables)
+    assert_false(detector.enabled?)
+  end
+
+  def test_apply
+    (@init_d + "milter-manager").open("w") do |file|
+      file << milter_manager_init_script
+    end
+    (@default_dir + "milter-manager").open("w") do |file|
+      file << <<-EOC
+CONNECTION_SPEC=inet:10025@localhost
+EOC
+    end
+
+    detector = debain_init_detector("milter-manager")
+    detector.detect
+    detector.apply(@loader)
+    assert_eggs([["milter-manager",
+                  "The milter that manages milters to combine them flexibly.",
+                  true,
+                  (@init_d + "milter-manager").to_s,
+                  "start",
+                  "inet:10025@localhost"]])
+  end
+
+  def test_apply_amavisd_new_milter_style
+    (@init_d + "amavisd-new-milter").open("w") do |file|
+      file << <<-EOM
+#! /bin/sh
+#
+# amavisd-new-milter    /etc/init.d/ initscript for amavisd-new milter
+# 		$Id: amavisd-new-milter.init 411 2004-05-15 04:45:59Z hmh $
+#
+#		Copyright (c) 2003 by Brian May <bam@debian.org>
+#			and Henrique M. Holschuh <hmh@debian.org>
+#		Distributed under the GPL version 2
+#
+### BEGIN INIT INFO
+# Provides:          amavisd-new-milter
+# Required-Start:    $syslog 
+# Required-Stop:     $syslog
+# Should-Start:      $local_fs
+# Should-Stop:       $local_fs
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Starts the milter for amavisd-new 
+# Description:       milter for sendmail/postfix and amavisd-new
+### END INIT INFO
+
+PATH=/sbin:/bin:/usr/sbin:/usr/bin
+DAEMON=/usr/sbin/amavis-milter
+PARENTDAEMON=/usr/sbin/amavisd-new
+NAME=amavis-milter
+DESC="AMaViS Daemons (milter)"
+PARAMS="-D -p /var/lib/amavis/amavisd-new-milter.sock"
+
+test -f ${PARENTDAEMON} || exit 0
+test -f ${DAEMON} || exit 0
+
+set -e
+
+START="--start --quiet --pidfile /var/run/amavis/amavisd-new-milter.pid --chuid amavis --startas ${DAEMON} --name ${NAME} -- ${PARAMS}"
+EOM
+    end
+
+    detector = debain_init_detector("amavisd-new-milter")
+    detector.detect
+    detector.apply(@loader)
+    assert_eggs([["amavis-milter",
+                  "milter for sendmail/postfix and amavisd-new",
+                  true,
+                  (@init_d + "amavisd-new-milter").to_s,
+                  "start",
+                  "unix:/var/lib/amavis/amavisd-new-milter.sock"]])
+  end
+
+  def test_apply_clamav_milter_style
+    (@init_d + "clamav-milter").open("w") do |file|
+      file << <<-EOM
+#!/bin/sh
+### BEGIN INIT INFO
+# Provides:          clamav-milter
+# Required-Start:    $syslog
+# Should-Start:      clamav-daemon
+# Required-Stop:
+# Should-Stop:       
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 6
+# Short-Description: ClamAV virus milter
+# Description:       Clam AntiVirus milter interface
+### END INIT INFO
+
+# edit /etc/default/clamav-milter for options
+
+PATH=/sbin:/bin:/usr/sbin:/usr/bin
+DAEMON=/usr/sbin/clamav-milter
+DESC="Sendmail milter plugin for ClamAV"
+BASENAME=clamav-milter
+CLAMAVCONF=/etc/clamav/clamd.conf
+DEFAULT=/etc/default/clamav-milter
+OPTIONS="-dq"
+SUPERVISOR=/usr/bin/daemon
+SUPERVISORPIDFILE="/var/run/clamav/daemon-clamav-milter.pid"
+SUPERVISORARGS="-F $SUPERVISORPIDFILE --name=$BASENAME --respawn"
+CLAMAVDAEMONUPGRADE="/var/run/clamav-daemon-being-upgraded"
+
+[ -x "$DAEMON" ] || exit 0
+[ -r "$CLAMAVCONF" ] || exit 0
+[ -r "$DEFAULT" ] && . $DEFAULT
+[ -z "$PIDFILE" ] && PIDFILE=/var/run/clamav/clamav-milter.pid
+[ -z "$SOCKET" ] && SOCKET=local:/var/run/clamav/clamav-milter.ctl
+EOM
+    end
+
+    detector = debain_init_detector("clamav-milter")
+    detector.detect
+    detector.apply(@loader)
+    assert_eggs([["clamav-milter",
+                  "Clam AntiVirus milter interface",
+                  true,
+                  (@init_d + "clamav-milter").to_s,
+                  "start",
+                  "local:/var/run/clamav/clamav-milter.ctl"]])
+  end
+
+  def test_apply_dkim_filter_style
+    (@init_d + "dkim-filter").open("w") do |file|
+      file << <<-EOM
+#! /bin/sh
+#
+### BEGIN INIT INFO
+# Provides:		dkim-filter
+# Required-Start:	$syslog
+# Required-Stop:	$syslog
+# Should-Start:		$local_fs $network
+# Should-Stop:		$local_fs $network
+# Default-Start:	2 3 4 5
+# Default-Stop:		0 1 6
+# Short-Description:	Start the DKIM Milter service
+### END INIT INFO
+
+PATH=/sbin:/bin:/usr/sbin:/usr/bin
+DAEMON=/usr/sbin/dkim-filter
+NAME=dkim-filter
+DESC="DKIM Filter"
+RUNDIR=/var/run/$NAME
+USER=dkim-filter
+GROUP=dkim-filter
+SOCKET=local:$RUNDIR/$NAME.sock
+PIDFILE=$RUNDIR/$NAME.pid
+EOM
+    end
+    (@default_dir + "dkim-filter").open("w") do |file|
+      file << <<-EOC
+SOCKET="inet:12345@192.0.2.1" # listen on 192.0.2.1 on port 12345
+EOC
+    end
+
+    detector = debain_init_detector("dkim-filter")
+    detector.detect
+    detector.apply(@loader)
+    assert_eggs([["dkim-filter",
+                  "DKIM Filter",
+                  true,
+                  (@init_d + "dkim-filter").to_s,
+                  "start",
+                  "inet:12345@192.0.2.1"]])
+  end
+
+  def test_apply_milter_greylist_style
+    (@init_d + "milter-greylist").open("w") do |file|
+      file << <<-EOM
+#! /bin/sh
+
+# Greylist init script
+# July 2004
+# BERTRAND Joël
+#
+### BEGIN INIT INFO
+# Provides:                 milter-greylist
+# Required-Start:    $local_fs $named $remote_fs $syslog
+# Required-Stop:     mountall
+# Should-Start:             sendmail
+# Should-Stop:
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Script to start/stop the greylist-milter
+# Description: another spam-defense service
+### END INIT INFO
+
+
+
+# Based on skeleton by Miquel van Smoorenburg and Ian Murdock
+
+PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin
+DAEMON=/usr/sbin/milter-greylist
+NAME=greylist
+SNAME=greylist
+DESC="Greylist Mail Filter Daemon"
+PIDFILE="/var/run/$NAME.pid"
+PNAME="milter-greylist"
+USER="greylist"
+SOCKET=/var/run/milter-greylist/milter-greylist.sock
+
+
+[ -x $DAEMON ] || DAEMON=/usr/bin/milter-greylist
+[ -x $DAEMON ] || exit 0
+
+
+export TMPDIR=/tmp
+# Apparently people have trouble if this isn't explicitly set...
+
+ENABLED=0
+OPTIONS=""
+NICE=
+
+test -f /etc/default/milter-greylist && . /etc/default/milter-greylist
+
+DOPTIONS="-P $PIDFILE -u $USER -p $SOCKET"
+EOM
+    end
+
+    detector = debain_init_detector("milter-greylist")
+    detector.detect
+    detector.apply(@loader)
+    assert_eggs([["greylist",
+                  "another spam-defense service",
+                  false,
+                  (@init_d + "milter-greylist").to_s,
+                  "start",
+                  "unix:/var/run/milter-greylist/milter-greylist.sock"]])
+  end
+
+  private
+  def debain_init_detector(name)
+    detector = Milter::Manager::ConfigurationLoader::DebianInitDetector.new(name)
+
+    _init_d = @init_d
+    _default_dir = @default_dir
+    singleton_object = class << detector; self; end
+    singleton_object.send(:define_method, :init_d) do
+      _init_d.to_s
+    end
+
+    singleton_object.send(:define_method, :default_dir) do
+      _default_dir.to_s
+    end
+
+    detector
+  end
+
+  def milter_manager_init_script
+    top = Pathname(File.dirname(__FILE__)) + ".." + ".." + ".." + ".."
+    (top + "data" + "init.d" + "milter-manager").read
+  end
+
+  def assert_eggs(expected_eggs)
+    egg_info = @configuration.eggs.collect do |egg|
+      [egg.name,
+       egg.description,
+       egg.enabled?,
+       egg.command,
+       egg.command_options,
+       egg.connection_spec]
+    end
+    assert_equal(expected_eggs, egg_info)
+  end
+end
