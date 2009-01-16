965e9c354c42976f72583858b6cf48b1e7a73778
ktou <ktou@2eb25379-3f72-4156-9665-29aef8dc9b31>
2009-01-16 07:16:07 +0000

    * add user.

--- admin/config/routes.rb
+++ admin/config/routes.rb
@@ -1,4 +1,12 @@
 ActionController::Routing::Routes.draw do |map|
+  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
+  map.login '/login', :controller => 'sessions', :action => 'new'
+  map.register '/register', :controller => 'users', :action => 'create'
+  map.signup '/signup', :controller => 'users', :action => 'new'
+  map.resources :users
+
+  map.resource :session
+
   map.namespace :config do |config|
     config.resources(:milters,
                      :member => {
