c76fe219ce08e7dddf95abc3ca1d8e39d049bc9a
ktou <ktou@2eb25379-3f72-4156-9665-29aef8dc9b31>
2009-01-27 06:58:43 +0000

    * login.

--- admin/test/functional/config/connections_controller_test.rb
+++ admin/test/functional/config/connections_controller_test.rb
@@ -1,42 +1,70 @@
 require 'test_helper'
 
 class Config::ConnectionsControllerTest < ActionController::TestCase
+  include AuthenticatedTestHelper
+
   def test_index
+    get(:index)
+    assert_redirected_to(new_session_path)
+
+    login_as(:aaron)
     assert_raise(ActionController::UnknownAction) do
       get(:index)
     end
   end
 
   def test_new
+    get(:new)
+    assert_redirected_to(new_session_path)
+
+    login_as(:aaron)
     assert_raise(ActionController::UnknownAction) do
       get(:new)
     end
   end
 
   def test_create
+    params = {:config_connection => {:spec => "inet:2929@localhost"}}
+    post(:create, params)
+    assert_redirected_to(new_session_path)
+
+    login_as(:aaron)
     assert_raise(ActionController::UnknownAction) do
-      post(:create, :config_connection => {:spec => "inet:2929@localhost"})
+      post(:create, params)
     end
   end
 
   def test_show
+    get(:show, :id => connections(:default).id)
+    assert_redirected_to(new_session_path)
+
+    login_as(:aaron)
     get(:show, :id => connections(:default).id)
     assert_response(:success)
   end
 
   def test_edit
     get(:edit, :id => connections(:default).id)
+    assert_redirected_to(new_session_path)
+
+    login_as(:aaron)
+    get(:edit, :id => connections(:default).id)
     assert_response(:success)
   end
 
   def test_update
-    put(:update,
-        :id => connections(:default).id,
-        :config_connection => {
-          :spec_type => "inet",
-          :spec_port => "2929",
-          :spec_host => "localhost"
-        })
+    params = {
+      :id => connections(:default).id,
+      :config_connection => {
+        :spec_type => "inet",
+        :spec_port => "2929",
+        :spec_host => "localhost"
+      }}
+    put(:update, params)
+    assert_redirected_to(new_session_path)
+
+    login_as(:aaron)
+    put(:update, params)
 
     connection = assigns(:connection)
     assert_redirected_to(connection)
@@ -44,8 +72,13 @@ class Config::ConnectionsControllerTest < ActionController::TestCase
   end
 
   def test_destroy
+    params = {:id => connections(:default).id}
+    delete(:destroy, params)
+    assert_redirected_to(new_session_path)
+
+    login_as(:aaron)
     assert_raise(ActionController::UnknownAction) do
-      delete(:destroy, :id => connections(:default).id)
+      delete(:destroy, params)
     end
   end
 end
