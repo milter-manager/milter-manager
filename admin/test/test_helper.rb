02b5683a837735df9ad569ca2441d67e34f6daff
ktou <ktou@2eb25379-3f72-4156-9665-29aef8dc9b31>
2009-01-27 06:01:04 +0000

    * follow the recent Webrat usage.

--- admin/test/test_helper.rb
+++ admin/test/test_helper.rb
@@ -6,7 +6,11 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 require 'test_help'
 
-require 'webrat/rails'
+require 'webrat'
+
+Webrat.configure do |config|
+  config.mode = :rails
+end
 
 class Test::Unit::TestCase
   # Transactional fixtures accelerate your tests by wrapping each test method
