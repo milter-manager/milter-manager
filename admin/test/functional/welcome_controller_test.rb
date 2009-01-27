ce11886051e7a9aa4f6a99a11e40314188494b5e
ktou <ktou@2eb25379-3f72-4156-9665-29aef8dc9b31>
2009-01-27 06:15:32 +0000

    * login.

--- admin/test/functional/welcome_controller_test.rb
+++ admin/test/functional/welcome_controller_test.rb
@@ -1,16 +1,28 @@
 require 'test_helper'
 
 class WelcomeControllerTest < ActionController::TestCase
+  include AuthenticatedTestHelper
+
+  def test_without_login
+    get(:index)
+    assert_redirected_to(new_session_path)
+  end
+
   def test_menu_before_connection_setup
+    login_as(:aaron)
     default_connection = Config::Connection.default
     default_connection.spec = nil
     default_connection.save(false)
-    get :index
+    get(:index)
+    assert_response(:success)
     assert_select("ul.header-menu a[href=#{control_path}]", 0)
   end
 
   def test_menu_after_connection_setup
-    get :index
-    assert_select("ul.header-menu a[href=#{control_path}]")
+    login_as(:aaron)
+    get(:index)
+    assert_response(:success)
+    # TODO: enable control page.
+    assert_select("ul.header-menu a[href=#{control_path}]", 0)
   end
 end
