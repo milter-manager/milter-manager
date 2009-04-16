f59877cb76c65386e974c95b25ce3c5f48f3334c
ktou <ktou@2eb25379-3f72-4156-9665-29aef8dc9b31>
2009-04-16 02:05:23 +0000

    * binding/ruby/test/manager/test-debian-init-detector.rb: follow   the data/init.d/debian/milter-manager.conf changes.

--- binding/ruby/test/manager/test-debian-init-detector.rb
+++ binding/ruby/test/manager/test-debian-init-detector.rb
@@ -90,6 +90,8 @@ class TestDebianInitDetector < Test::Unit::TestCase
     detector.detect
     assert_equal({
                    "Provides" => "milter-manager",
+                   "Required-Start" => "$local_fs $remote_fs $network $syslog",
+                   "Required-Stop" => "$local_fs $remote_fs $network $syslog",
                    "Default-Start" => "2 3 4 5",
                    "Default-Stop" => "0 1 6",
                    "Short-Description" => "milter-manager's init script",
