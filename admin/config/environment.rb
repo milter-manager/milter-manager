328cdafcbbf4a831486da13af9347bda6e4affb5
ktou <ktou@2eb25379-3f72-4156-9665-29aef8dc9b31>
2009-05-12 14:13:00 +0000

    * admin/config/environment.rb: specify gem version.

--- admin/config/environment.rb
+++ admin/config/environment.rb
@@ -27,11 +27,12 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem "locale_rails"
+  config.gem "locale_rails"#, :version => "2.0.4"
   # We want to use rails-exception_notification. :<
   config.gem "jeremyevans-exception_notification",
              :lib => "exception_notification",
-             :source => "http://gems.github.com/"
+             :source => "http://gems.github.com/",
+             :version => "1.0.20081208"
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
