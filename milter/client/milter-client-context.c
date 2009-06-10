fb77f72d4b7dc7e98c50a13229a7d3577eb65572
ktou <ktou@2eb25379-3f72-4156-9665-29aef8dc9b31>
2009-06-10 02:05:25 +0000

    * milter/client/milter-client-context.c: log abort response.

--- milter/client/milter-client-context.c
+++ milter/client/milter-client-context.c
@@ -2643,11 +2643,24 @@ static void
 abort_response (MilterClientContext *context, MilterStatus status)
 {
     MilterClientContextPrivate *priv;
+    gchar *state_name, *status_name;
 
     if (status == MILTER_STATUS_DEFAULT || status == MILTER_STATUS_NOT_CHANGE)
         status = MILTER_STATUS_ABORT;
 
     priv = MILTER_CLIENT_CONTEXT_GET_PRIVATE(context);
+
+    state_name =
+        milter_utils_get_enum_nick_name(MILTER_TYPE_CLIENT_CONTEXT_STATE,
+                                        priv->state);
+    status_name = milter_utils_get_enum_nick_name(MILTER_TYPE_STATUS, status);
+    milter_debug("[%u] [client][reply][abort][%s][%s]",
+                 milter_agent_get_tag(MILTER_AGENT(context)),
+                 state_name,
+                 status_name);
+    g_free(status_name);
+    g_free(state_name);
+
     if (MILTER_STATUS_IS_PASS(priv->status) &&
         (MILTER_CLIENT_CONTEXT_STATE_NEGOTIATE <= priv->state &&
          priv->state < MILTER_CLIENT_CONTEXT_STATE_END_OF_MESSAGE)) {
