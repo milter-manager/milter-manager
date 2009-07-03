3f5f8d238f2a445e7e8721c68504923c587ddffa
ktou <ktou@2eb25379-3f72-4156-9665-29aef8dc9b31>
2009-07-03 01:32:29 +0000

    * admin/config/environment.rb: specify locale_rails version.

--- admin/config/environment.rb
+++ admin/config/environment.rb
@@ -27,7 +27,7 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem "locale_rails"#, :version => "2.0.4"
+  config.gem "locale_rails", :version => "2.0.4"
   # We want to use rails-exception_notification. :<
   config.gem "jeremyevans-exception_notification",
              :lib => "exception_notification",
