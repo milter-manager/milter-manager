7d5e51e31c722ddfa000eaa65fef882b361e96b8
ktou <ktou@2eb25379-3f72-4156-9665-29aef8dc9b31>
2009-08-26 13:10:32 +0000

    * admin/config/environment.rb: jeremyevans-exception_notification:   1.0.20081208 -> 1.0.20090728.

--- admin/config/environment.rb
+++ admin/config/environment.rb
@@ -32,7 +32,7 @@ Rails::Initializer.run do |config|
   config.gem "jeremyevans-exception_notification",
              :lib => "exception_notification",
              :source => "http://gems.github.com/",
-             :version => "1.0.20081208"
+             :version => "1.0.20090728"
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
