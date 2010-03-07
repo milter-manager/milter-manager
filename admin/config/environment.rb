71453cbb405e49468685a76df753442477a36dfe
ktou <ktou@2eb25379-3f72-4156-9665-29aef8dc9b31>
2010-03-07 11:48:42 +0000

    * admin/config/environment.rb: Rails 2.3.4 -> 2.3.5.

--- admin/config/environment.rb
+++ admin/config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -27,12 +27,8 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem "locale_rails", :version => "2.0.4"
-  # We want to use rails-exception_notification. :<
-  config.gem "jeremyevans-exception_notification",
-             :lib => "exception_notification",
-             :source => "http://gems.github.com/",
-             :version => "1.0.20090728"
+  config.gem "locale_rails", :version => "2.0.5"
+  config.gem "exception_notification", :version => "1.0.20090728"
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
