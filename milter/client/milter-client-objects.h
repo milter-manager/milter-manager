ab11dad9e8bdac551d495b62cde93d75cda5b8c6
ktou <ktou@2eb25379-3f72-4156-9665-29aef8dc9b31>
2010-03-21 13:52:35 +0000

    * milter/client/milter-client-objects.h: add a missing file.

---
+++ milter/client/milter-client-objects.h
@@ -0,0 +1,36 @@
+/* -*- Mode: C; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ *  Copyright (C) 2010  Kouhei Sutou <kou@clear-code.com>
+ *
+ *  This library is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef __MILTER_CLIENT_OBJECTS_H__
+#define __MILTER_CLIENT_OBJECTS_H__
+
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+typedef struct _MilterClient         MilterClient;
+typedef struct _MilterClientContext  MilterClientContext;
+
+G_END_DECLS
+
+#endif /* __MILTER_CLIENT_OBJECTS_H__ */
+
+/*
+vi:nowrap:ai:expandtab:sw=4
+*/
