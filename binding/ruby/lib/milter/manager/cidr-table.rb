2d5ab1878d8471c2dac4a4c52701edd52cbd5f7c
ktou <ktou@2eb25379-3f72-4156-9665-29aef8dc9b31>
2010-08-05 14:33:29 +0000

    * binding/ruby/lib/milter/manager.rb,   binding/ruby/lib/milter/manager/cidr-table.rb,   binding/ruby/lib/milter/manager/exception.rb,   binding/ruby/test/manager/test-cidr-table.rb: add Postfix   cidr_table(5) parser.

---
+++ binding/ruby/lib/milter/manager/cidr-table.rb
@@ -0,0 +1,98 @@
+# Copyright (C) 2010  Kouhei Sutou <kou@clear-code.com>
+#
+# This library is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
+require 'English'
+
+module Milter::Manager
+  class CIDRTable
+    class InvalidValueError < ParseError
+      attr_reader :value, :detail, :line, :path, :line_no
+      def initialize(value, detail, line, path, line_no)
+        @value = value
+        @detail = detail
+        @line = line
+        @path = path
+        @line_no = line_no
+        super("#{@path}:#{@line_no}: #{detail}: <#{value}>: <#{line}>")
+      end
+    end
+
+    class InvalidFormatError < ParseError
+      attr_reader :line, :path, :line_no
+      def initialize(line, path, line_no)
+        @line = line
+        @path = path
+        @line_no = line_no
+        super("#{@path}:#{@line_no}: invalid format <#{line}>")
+      end
+    end
+
+    def initialize
+      @table = []
+    end
+
+    def parse(io)
+      each_line(io) do |line|
+        case line
+        when /\A\s*([\d\.]+|[\da-fA-F:]+)(?:\/(\d+))?\s+([\da-zA-Z]+)\s*$/
+          address = $1
+          network = $2
+          action = $3
+          address << "/#{network}" unless network.nil?
+          ip_address = nil
+          begin
+            ip_address = IPAddr.new(address)
+          rescue ArgumentError
+            raise InvalidValueError.new(address, $!.message, line,
+                                        io.path, io.lineno)
+          end
+          @table << [ip_address, action]
+        else
+          raise InvalidFormatError.new(line, io.path, io.lineno)
+        end
+      end
+    end
+
+    def find(address)
+      address = address.to_ip_address if address.respond_to?(:to_ip_address)
+      @table.each do |match_address, action|
+        return action if match_address === address
+      end
+      nil
+    end
+
+    private
+    def each_line(io)
+      current_line = nil
+      io.each_line do |line|
+        case line
+        when /\A\s*#/, /\A\s*$/
+        when /\A\s+/
+          target_line = $POSTMATCH.chomp
+          if current_line
+            current_line << " "
+          else
+            current_line = ""
+          end
+          current_line << target_line
+        else
+          yield(current_line) if current_line
+          current_line = line.chomp
+        end
+      end
+      yield(current_line) if current_line
+    end
+  end
+end
---
+++ binding/ruby/test/manager/test-cidr-table.rb
@@ -0,0 +1,161 @@
+# Copyright (C) 2010  Kouhei Sutou <kou@clear-code.com>
+#
+# This library is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
+class TestCIDRTable < Test::Unit::TestCase
+  def setup
+    @table = Milter::Manager::CIDRTable.new
+  end
+
+  def test_empty
+    assert_nil(@table.find(ipv4("127.0.0.1")))
+  end
+
+  def test_exact_match_ipv4
+    @table.parse(StringIO.new(<<-EOC))
+# Rule order matters. Put more specific whitelist entries
+# before more general blacklist entries.
+192.168.1.1             OK
+192.168.1.0/24          REJECT
+EOC
+    assert_equal("OK", @table.find(ipv4("192.168.1.1")))
+  end
+
+  def test_network_match_ipv4
+    @table.parse(StringIO.new(<<-EOC))
+# Rule order matters. Put more specific whitelist entries
+# before more general blacklist entries.
+192.168.1.1             OK
+192.168.1.0/24          REJECT
+EOC
+    assert_equal("REJECT", @table.find(ipv4("192.168.1.29")))
+  end
+
+  def test_all_match_ipv4
+    @table.parse(StringIO.new(<<-EOC))
+# Rule order matters. Put more specific whitelist entries
+# before more general blacklist entries.
+0.0.0.0/0               OK
+192.168.1.1             REJECT
+192.168.1.0/24          REJECT
+EOC
+    assert_equal("OK", @table.find(ipv4("192.168.1.1")))
+  end
+
+  def test_exact_match_ipv6
+    @table.parse(StringIO.new(<<-EOC))
+# Rule order matters. Put more specific whitelist entries
+# before more general blacklist entries.
+2001:2f8:c2:201::fff0 OK
+2001:2f8:c2:201::0/64 REJECT
+EOC
+    assert_equal("OK", @table.find(ipv6("2001:2f8:c2:201::fff0")))
+  end
+
+  def test_network_match_ipv6
+    @table.parse(StringIO.new(<<-EOC))
+# Rule order matters. Put more specific whitelist entries
+# before more general blacklist entries.
+2001:2f8:c2:201::fff0 OK
+2001:2f8:c2:201::0/64 REJECT
+EOC
+    assert_equal("REJECT", @table.find(ipv4("2001:2f8:c2:201::1")))
+  end
+
+  def test_all_match_ipv6
+    @table.parse(StringIO.new(<<-EOC))
+# Rule order matters. Put more specific whitelist entries
+# before more general blacklist entries.
+::/0                  OK
+2001:2f8:c2:201::fff0 REJECT
+2001:2f8:c2:201::0/64 REJECT
+EOC
+    assert_equal("OK", @table.find(ipv6("2001:2f8:c2:201::fff0")))
+  end
+
+  def test_logical_line
+    @table.parse(StringIO.new(<<-EOC))
+192.168.1.1
+  OK
+EOC
+    assert_equal("OK", @table.find(ipv4("192.168.1.1")))
+  end
+
+  def test_logical_line_with_empty_line
+    @table.parse(StringIO.new(<<-EOC))
+192.168.1.1
+
+  OK
+EOC
+    assert_equal("OK", @table.find(ipv4("192.168.1.1")))
+  end
+
+  def test_logical_line_with_comment_line
+    @table.parse(StringIO.new(<<-EOC))
+192.168.1.1
+# comment line
+  OK
+EOC
+    assert_equal("OK", @table.find(ipv4("192.168.1.1")))
+  end
+
+  def test_invalid_address_ipv4
+    error = Milter::Manager::CIDRTable::InvalidValueError.new("192.168.1",
+                                                              "invalid address",
+                                                              "192.168.1 OK",
+                                                              nil,
+                                                              1)
+    assert_raise(error) do
+      @table.parse(StringIO.new(<<-EOC))
+192.168.1 OK
+EOC
+    end
+    assert_nil(@table.find(ipv4("192.168.1.1")))
+  end
+
+  def test_invalid_address_ipv6
+    error = Milter::Manager::CIDRTable::InvalidValueError.new("f:",
+                                                              "invalid address",
+                                                              "f: OK",
+                                                              nil,
+                                                              1)
+    assert_raise(error) do
+      @table.parse(StringIO.new(<<-EOC))
+f: OK
+EOC
+    end
+    assert_nil(@table.find(ipv4("192.168.1.1")))
+  end
+
+  def test_invalid_format
+    error = Milter::Manager::CIDRTable::InvalidFormatError.new("x:: OK",
+                                                               nil,
+                                                               1)
+    assert_raise(error) do
+      @table.parse(StringIO.new(<<-EOC))
+x:: OK
+EOC
+    end
+    assert_nil(@table.find(ipv4("192.168.1.1")))
+  end
+
+  private
+  def ipv4(address, port=2929)
+    Milter::SocketAddress::IPv4.new(address, port)
+  end
+
+  def ipv6(address, port=2929)
+    Milter::SocketAddress::IPv6.new(address, port)
+  end
+end
