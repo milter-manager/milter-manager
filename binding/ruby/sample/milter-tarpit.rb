992ff6b1b3106d22eb5e8384e641047a91522dd1
ktou <ktou@2eb25379-3f72-4156-9665-29aef8dc9b31>
2010-09-14 13:51:20 +0000

    * binding/ruby/sample/milter-tarpit.rb: add a sample tarpitting milter.

---
+++ binding/ruby/sample/milter-tarpit.rb
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2010  Kouhei Sutou <kou@clear-code.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+require 'milter'
+
+class MilterTarpit < Milter::ClientSession
+  def helo(fqdn)
+    tarpitting = true
+    GLib::Timeout.add(5000) do
+      tarpitting = false
+      false
+    end
+    main_context = GLib::MainContext.default
+    while tarpitting
+      main_context.iteration(true)
+    end
+  end
+end
+
+command_line = Milter::Client::CommandLine.new
+command_line.run do |client, _options|
+  client.register(MilterTarpit)
+end
