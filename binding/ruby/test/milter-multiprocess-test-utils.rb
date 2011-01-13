c1863f8d19dcfe2c972bbd130ffcab947ecde2c1
n0kada <n0kada@2eb25379-3f72-4156-9665-29aef8dc9b31>
2011-01-13 08:43:17 +0000

    * binding/ruby/test/milter-multiprocess-test-utils.rb (MilterMultiprocessTestUtils#setup_workers): set n_workers to argument, not instance variable, and set @n_workers always.

--- binding/ruby/test/milter-multiprocess-test-utils.rb
+++ binding/ruby/test/milter-multiprocess-test-utils.rb
@@ -3,9 +3,13 @@ module MilterMultiprocessTestUtils
 
   private
   def setup_workers(client)
-    if n_workers = ENV["MILTER_N_WORKERS"] and (n_workers = Integer(n_workers)) > 0
-      @n_workers = n_workers
-      @client.n_workers = n_workers
+    if n_workers = ENV["MILTER_N_WORKERS"]
+      n_workers = Integer(n_workers)
+    end
+    n_workers ||= 0
+    @n_workers = n_workers
+    if n_workers > 0
+      client.n_workers = n_workers
     end
   end
 end
