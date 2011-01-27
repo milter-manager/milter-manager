3c8aec5b6fde781cf0fa9ae418b253497a57e696
Kouhei Sutou <kou@clear-code.com>
2011-01-27 23:18:44 +0900

    split package list to other file.

--- solaris/setup.sh
+++ solaris/setup.sh
@@ -2,6 +2,18 @@
 
 PATH=/usr/local/bin:/opt/csw/bin:/usr/sfw/bin:$PATH
 
+run()
+{
+    $@
+    if test $? -ne 0; then
+	echo "Failed $@"
+	exit 1
+    fi
+}
+
+base_dir=$(dirname $0)
+base_packages=$(cat "$base_dir/base-packages.list")
+
 pkgadd -d http://mirror.opencsw.org/opencsw/pkg_get.pkg
 
-pkg-get install sudo
+pkg-get install $(base_pacakges)
