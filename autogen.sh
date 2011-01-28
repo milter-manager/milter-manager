ccf5e5f13109d6c2d84371f84972d7f3018eb46a
Kouhei Sutou <kou@clear-code.com>
2011-01-28 16:38:20 +0900

    export test-unit in autogen.sh.

--- autogen.sh
+++ autogen.sh
@@ -20,6 +20,9 @@ svn export --force ${cutter_repository}/trunk/misc
 clear_code_repository=http://www.clear-code.com/repos/svn
 svn export --force ${clear_code_repository}/tdiary html/blog/clear-code
 
+test_unit_repository=http://test-unit.rubyforge.org/svn
+svn export --force ${test_unit_repository}/trunk binding/ruby/test-unit
+
 run ${ACLOCAL:-aclocal} $ACLOCAL_OPTIONS
 run ${LIBTOOLIZE:-libtoolize} --copy --force
 run ${INTLTOOLIZE:-intltoolize} --force --copy
