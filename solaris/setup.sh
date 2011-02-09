de68398fb357095139b3ea45be77b6f7037c192f
Kenji Okimoto <okimoto@clear-code.com>
2011-02-09 13:53:33 +0900

    rename function inst -> setup.

--- solaris/setup.sh
+++ solaris/setup.sh
@@ -1,21 +1,14 @@
 #!/usr/bin/bash
 
 PATH=/usr/local/bin:/opt/csw/bin:/usr/sfw/bin:$PATH
-PREFIX=/opt/local
+PREFIX=$HOME/opt
 
 AR=/usr/ccs/bin/ar
 MAKE="/usr/sfw/bin/gmake -j4"
 CC=/usr/sfw/bin/gcc
 CFLAGS=-m64
 
-run()
-{
-    $@
-    if test $? -ne 0; then
-	echo "Failed $@"
-	exit 1
-    fi
-}
+test -f ./functions && source ./functions
 
 base_dir=$(dirname $0)
 base_packages=$(cat "$base_dir/base-packages.list")
@@ -25,55 +18,12 @@ if test ! -x /opt/csw/bin/pkg-get; then
     run pkgadd -d http://mirror.opencsw.org/opencsw/pkg_get.pkg
 fi
 run pkg-get install gnupg
-wget -O /tmp/gpg.key http://www.opencsw.org/get-it/mirrors/
-gpg --import /tmp/gpg.key
+run wget -O /tmp/gpg.key http://www.opencsw.org/get-it/mirrors/
+run gpg --import /tmp/gpg.key
 
-mkdir -p /.gnupg
-if ! grep search.keyserver.net /.gnupg/options; then
-    echo keyserver search.keyserver.net >>/.gnupg/options
-fi
-if ! grep search.keyserver.net /.gnupg/gpg.conf; then
-    echo keyserver search.keyserver.net >>/.gnupg/gpg.conf
-fi
 run pkg-get -U
 
 run pkg-get install "$base_packages"
 
-inst()
-{
-    url="$1"
-    tarball="${1##*/}"
-    base="${tarball%.tar.*}"
-    build_dir="${PREFIX}/build"
-    shift
-
-    mkdir -p "$build_dir"
-
-
-    echo -n "Downloading ${base}..."
-    run wget -N -P "${SOURCES}" "$url"
-    echo done.
-
-    echo -n "Extracting ${base}..."
-    run gtar xf "${SOURCES}/${tarball}" -C "${build_dir}"
-    echo done.
-
-    echo -n "Configuring ${base}..."
-    (
-        cd "${build_dir}/${base}"
-        run ./configure --enable-shared --prefix="${PREFIX}" "$@"
-    )
-    echo done.
-
-    echo -n "Building ${base}..."
-    run ${MAKE} -C "${build_dir}/${base}"
-    echo done.
-
-    echo -n "Installing ${base}..."
-    run ${MAKE} -C "${build_dir}/${base}" prefix="${PREFIX}" install
-    echo done.
-}
-
-test -f ./development_sourcelist && source ./development_sourcelist
 
 echo done.
