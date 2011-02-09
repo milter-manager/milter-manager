67283cfb2797323685affef5424f50f23f7720a9
Kenji Okimoto <okimoto@clear-code.com>
2011-02-09 11:53:19 +0900

    install base packages (sudo, ggrep, git, subversion).

--- solaris/setup.sh
+++ solaris/setup.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/bash
 
 PATH=/usr/local/bin:/opt/csw/bin:/usr/sfw/bin:$PATH
+PREFIX=/opt/local
+
+AR=/usr/ccs/bin/ar
+MAKE="/usr/sfw/bin/gmake -j4"
+CC=/usr/sfw/bin/gcc
+CFLAGS=-m64
 
 run()
 {
@@ -13,7 +19,27 @@ run()
 
 base_dir=$(dirname $0)
 base_packages=$(cat "$base_dir/base-packages.list")
+SOURCES="${base_dir}/sources"
 
-pkgadd -d http://mirror.opencsw.org/opencsw/pkg_get.pkg
+if test ! -x /opt/csw/bin/pkg-get; then
+    run pkgadd -d http://mirror.opencsw.org/opencsw/pkg_get.pkg
+fi
+run pkg-get install gnupg
+wget -O /tmp/gpg.key http://www.opencsw.org/get-it/mirrors/
+gpg --import /tmp/gpg.key
 
-pkg-get install $(base_pacakges)
+mkdir -p /.gnupg
+if ! grep search.keyserver.net /.gnupg/options; then
+    echo keyserver search.keyserver.net >>/.gnupg/options
+fi
+if ! grep search.keyserver.net /.gnupg/gpg.conf; then
+    echo keyserver search.keyserver.net >>/.gnupg/gpg.conf
+fi
+run pkg-get -U
+
+for package in "$base_packages"; do
+    run pkg-get install $package
+done
+
+
+echo done.
