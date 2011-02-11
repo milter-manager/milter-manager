99928b85ddfc507ecddf67525fc2c04a9e218f79
Kouhei Sutou <kou@clear-code.com>
2011-02-11 17:24:35 +0900

    install Ruby after iconv is installed.

--- solaris/install-libraries.sh
+++ solaris/install-libraries.sh
@@ -7,8 +7,6 @@ source ./functions.sh
 
 echo "$(time_stamp): Installing libraries..."
 
-install_package ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.2-p136.tar.bz2 \
-    --disable-install-doc --enable-shared
 install_package http://ftp.gnu.org/pub/gnu/gettext/gettext-0.18.1.1.tar.gz \
     --enable-relocatable --without-git
 install_package http://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.13.1.tar.gz
@@ -16,6 +14,9 @@ install_package http://ftp.gnu.org/pub/gnu/gettext/gettext-0.18.1.1.tar.gz \
     --enable-relocatable --without-git
 install_package http://ftp.gnome.org/pub/gnome/sources/glib/2.22/glib-2.22.5.tar.bz2 \
     --with-libiconv=gnu
+
+install_package ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.2-p136.tar.bz2 \
+    --disable-install-doc --enable-shared
 
 install_package http://downloads.sourceforge.net/cutter/cutter-1.1.6.tar.gz
 
