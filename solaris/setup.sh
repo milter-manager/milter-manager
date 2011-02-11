a45ea86e728f1ad6e3505e789842905af2bb855a
Kouhei Sutou <kou@clear-code.com>
2011-02-11 14:26:17 +0900

    set CPPFLAGS and LDFLAGS.

--- solaris/setup.sh
+++ solaris/setup.sh
@@ -10,6 +10,8 @@ export MAKE="/usr/sfw/bin/gmake -j4"
 export CC="/usr/sfw/bin/gcc -m64"
 export CFLAGS=-m64
 export CXXFLAGS=-m64
+export CPPFLAGS="-I$PREFIX/include"
+export LDFLAGS="-L$PREFIX/lib"
 export LD_LIBRARY_PATH=$PREFIX/lib:$LD_LIBRARY_PATH
 
 source ./functions.sh
