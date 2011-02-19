81cfc417dd2326447685c516a57ec1564865231c
Kouhei Sutou <kou@clear-code.com>
2011-02-19 20:39:02 +0900

    add -m64 to CC because Ruby doesn't use CFLAGS...

--- solaris/install-libraries.sh
+++ solaris/install-libraries.sh
@@ -9,6 +9,7 @@ echo "$(time_stamp): Installing libraries..."
 
 gnu_org="http://ftp.gnu.org/pub/gnu"
 gnome_org="http://ftp.gnome.org/pub/gnome/sources"
+ruby_lang_org="ftp://ftp.ruby-lang.org/pub/ruby"
 mysql_downloads="http://ftp.jaist.ac.jp/pub/mysql/Downloads"
 
 if test "$USE_OPEN_CSW" != "yes"; then
@@ -27,7 +28,8 @@ fi
 CC="$CC -m64" \
     install_package ${mysql_downloads}/MySQL-5.5/mysql-5.5.9.tar.gz
 
-install_package ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.2-p180.tar.bz2 \
+CC="$CC -m64" \
+    install_package ${ruby_lang_org}/1.9/ruby-1.9.2-p180.tar.bz2 \
     --disable-install-doc --enable-shared
 
 install_package http://downloads.sourceforge.net/cutter/cutter-1.1.7.tar.gz
