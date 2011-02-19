bbf49378cb12981ab16d95e1f1920237da686479
Kouhei Sutou <kou@clear-code.com>
2011-02-20 00:22:33 +0900

    build MySQL package.

--- solaris/build-pkgs.sh
+++ solaris/build-pkgs.sh
@@ -16,6 +16,7 @@ build_pkg ruby
 build_pkg iconv
 build_pkg gettext
 build_pkg glib
+build_pkg mysql
 build_pkg milter-manager
 echo "$(time_stamp): done."
 
@@ -26,5 +27,6 @@ pkgtrans -s $PKGS \
     "${PKG_PREFIX}iconv" \
     "${PKG_PREFIX}gettext" \
     "${PKG_PREFIX}glib" \
+    "${PKG_PREFIX}mysql" \
     "${PKG_PREFIX}milter-manager"
 echo "$(time_stamp): done."
