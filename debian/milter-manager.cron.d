730bc1050e426f67af65bcce77b8f6e59bb20899
Kouhei Sutou <kou@clear-code.com>
2011-03-11 00:16:33 +0900

    copy milter-manger.{init,default,cron.d} on build.

--- debian/milter-manager.cron.d
+++
@@ -1,2 +0,0 @@
-PATH=/bin:/usr/local/bin:/usr/bin
-*/5 * * * * root cat /var/log/mail.info | su milter-manager -s /bin/sh -c "milter-manager-log-analyzer --output-directory ~milter-manager/public_html/log"
