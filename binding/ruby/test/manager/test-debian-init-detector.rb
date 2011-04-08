ecb9d38f5c5d3c5a0540bc3b066a81f01c24e50e
Kouhei Sutou <kou@clear-code.com>
2011-04-08 12:45:31 +0900

    [debian] use the recommended option: --unix-socket-group.

--- binding/ruby/test/manager/test-debian-init-detector.rb
+++ binding/ruby/test/manager/test-debian-init-detector.rb
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2008-2010  Kouhei Sutou <kou@clear-code.com>
+# Copyright (C) 2008-2011  Kouhei Sutou <kou@clear-code.com>
 #
 # This library is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
@@ -72,7 +72,7 @@ class TestDebianInitDetector < Test::Unit::TestCase
                                      "--connection-spec", "",
                                      "--user-name", "",
                                      "--group-name", "",
-                                     "--socket-group-name", "",
+                                     "--unix-socket-group", "",
                                     ].join(" ") + " ",
                    "OPTION_ARGS" => "",
                    "RETVAL" => "$?",
@@ -131,7 +131,7 @@ EOC
                                      "--connection-spec", "",
                                      "--user-name", "",
                                      "--group-name", "",
-                                     "--socket-group-name", "",
+                                     "--unix-socket-group", "",
                                     ].join(" ") + " ",
                    "OPTION_ARGS" => "",
                    "RETVAL" => "$?",
