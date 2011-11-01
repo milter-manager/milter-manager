07cf735d488db9493b456f2d2d0eab35387a7643
Kenji Okimoto <okimoto@clear-code.com>
2011-11-01 19:30:20 +0900

    [admin] add config.gem.

--- admin/config/environment.rb
+++ admin/config/environment.rb
@@ -28,6 +28,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "exception_notification", :version => "2.3.3.0"
+  config.gem "sqlite3"
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
