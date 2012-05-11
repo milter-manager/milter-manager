0bd5185b537d3538f260921ca0e525b116e997bf
Kenji Okimoto <okimoto@clear-code.com>
2012-05-11 12:48:55 +0900

    package yum: receive GPG public key if not imported yet

--- package/yum/gpg-public-key.sh
+++ package/yum/gpg-public-key.sh
@@ -2,4 +2,9 @@
 
 script_base_dir=`dirname $0`
 
+gpg --fingerprint > /dev/null
+if test $? -nq 0; then
+    gpg --keyserver pgp.mit.edu --recv-keys `$script_base_dir/gpg-uid.sh`
+fi
+
 gpg -a --export `$script_base_dir/gpg-uid.sh`
