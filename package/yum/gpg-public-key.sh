92037cb60dbbc6bdf0da2c522bcf6a8f18843303
Kenji Okimoto <okimoto@clear-code.com>
2012-05-11 18:20:58 +0900

    package yum: add a missing parameter

--- package/yum/gpg-public-key.sh
+++ package/yum/gpg-public-key.sh
@@ -3,7 +3,7 @@
 script_base_dir=`dirname $0`
 key_id=`$script_base_dir/gpg-uid.sh`
 
-gpg --fingerprint > /dev/null
+gpg --fingerprint $key_id > /dev/null
 if test $? -nq 0; then
     gpg --keyserver pgp.mit.edu --recv-keys $key_id
 fi
