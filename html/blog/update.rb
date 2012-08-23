d190a3d526b37ab8414013e9e9b8643f2d531b3d
Kenji Okimoto <okimoto@clear-code.com>
2012-08-23 13:48:57 +0900

    blog: Fix blog script to specify sf_user.
    see GitHub: #11

--- html/blog/update.rb
+++ html/blog/update.rb
@@ -2,59 +2,78 @@
 
 require 'pathname'
 require 'fileutils'
+require 'optparse'
 
-if ARGV.delete("-h") or ARGV.delete("--help")
-  puts "Usage: #{$0} [--upload]"
-  puts "  e.g.: #{$0}          # only update"
-  puts "      : #{$0} --upload # update and upload"
-  exit
+def main
+  sf_user = nil
+  parser = OptionParser.new
+  parser.on("--upload=[USER]", "Update and upload") do |user|
+    sf_user = user
+  end
+
+  begin
+    parser.parse!
+  rescue OptionParser::ParseError => ex
+    $stderr.puts ex.message
+    $stderr.puts parser.help
+    exit 1
+  end
+
+  tdiary_dir_map = setup_tdiary_dirs(Pathname("~/work/ruby/tdiary").expand_path)
+
+  update(tdiary_dir_map)
+
+  if sf_user
+    run("./upload.rb", "--sf-user=#{sf_user}", tdiary_dir_map[:tdiary_compiled_dir].to_s)
+  end
 end
 
-upload = ARGV[0] == "--upload"
+def setup_tdiary_dirs(tdiary_base_dir)
+  tdiary_dir = tdiary_base_dir
+  tdiary_clear_code_public_dir = tdiary_base_dir + "clear-code" + "public"
+  blog_base_dir = nil
+  tdiary_conf_dir = nil
+  tdiary_conf = nil
+  ["~/public_html/milter-manager"].each do |dir|
+    conf = "#{dir}/blog/ja/tdiary.conf"
+    conf = Pathname(conf).expand_path
+    if conf.exist?
+      blog_base_dir = Pathname(dir).expand_path
+      tdiary_conf = conf
+      tdiary_conf_dir = conf.dirname
+      break
+    end
+  end
+  tdiary_compiled_dir = blog_base_dir + "blog-html/ja"
+
+  data_path_line = tdiary_conf.read.lines.grep(/^@data_path =/).join
+  tdiary_data_path = eval(data_path_line)
+  {
+    :tdiary_dir                   => tdiary_dir,
+    :tdiary_clear_code_public_dir => tdiary_clear_code_public_dir,
+    :tdiary_conf_dir              => tdiary_conf_dir,
+    :tdiary_compiled_dir          => tdiary_compiled_dir,
+    :tdiary_data_path             => tdiary_data_path,
+  }
+end
+
+def update(hash)
+  tdiary_clear_code_public_dir = hash[:tdiary_clear_code_public_dir]
+  tdiary_compiled_dir          = hash[:tdiary_compiled_dir]
+  tdiary_dir                   = hash[:tdiary_dir]
+  tdiary_conf_dir              = hash[:tdiary_conf_dir]
+  Dir.chdir(tdiary_clear_code_public_dir.to_s) do
+    FileUtils.rm_rf(tdiary_compiled_dir.to_s)
+    run("./html-archiver.rb",
+        "-t", tdiary_dir.to_s, "-c", tdiary_conf_dir.to_s,
+        tdiary_compiled_dir.to_s)
+  end
+end
 
 def run(*args)
   puts "[#{Dir.pwd}] #{args.join(' ')}"
   system(*args) or exit(1)
 end
 
-tdiary_base_dir = Pathname("~/work/ruby/tdiary").expand_path
-tdiary_dir = tdiary_base_dir + "core"
-tdiary_clear_code_public_dir = tdiary_base_dir + "clear-code" + "public"
-blog_base_dir = nil
-tdiary_conf_dir = nil
-tdiary_conf = nil
-["~/public_html/milter-manager"].each do |dir|
-  conf = "#{dir}/blog/ja/tdiary.conf"
-  conf = Pathname(conf).expand_path
-  if conf.exist?
-    blog_base_dir = Pathname(dir).expand_path
-    tdiary_conf = conf
-    tdiary_conf_dir = conf.dirname
-    break
-  end
-end
-tdiary_compiled_dir = blog_base_dir + "blog-html/ja"
+main
 
-data_path_line = tdiary_conf.read.grep(/^@data_path =/).join
-tdiary_data_path = eval(data_path_line)
-
-milter_manager_doc_dir = Pathname("~/work/c/milter-manager-doc").expand_path
-
-Dir.chdir(tdiary_data_path.to_s) do
-  run("git", "pull", "--rebase")
-end
-
-Dir.chdir(tdiary_clear_code_public_dir.to_s) do
-  run("svn", "up")
-  FileUtils.rm_rf(tdiary_compiled_dir.to_s)
-  run("./html-archiver.rb",
-      "-t", tdiary_dir.to_s, "-c", tdiary_conf_dir.to_s,
-      tdiary_compiled_dir.to_s)
-end
-
-if upload
-  Dir.chdir(milter_manager_doc_dir.to_s) do
-    run("git", "pull", "--rebase")
-    run("./upload.rb", tdiary_compiled_dir.to_s)
-  end
-end
--- html/blog/upload.rb
+++ html/blog/upload.rb
@@ -2,31 +2,43 @@
 
 require 'net/ftp'
 require 'pathname'
+require 'optparse'
 
-if ARGV.include?("-h") or ARGV.include?("--help")
-  puts "Usage: #{$0} [BLOG_HTML_DIR]"
-  puts "  e.g.: #{$0} blog-html"
-  exit
-end
-
-tdiary_compiled_dir = ARGV.shift
-if tdiary_compiled_dir.nil?
-  tdiary_conf_dir = nil
-  ["~/public_html/milter-manager"].each do |dir|
-    conf = "#{dir}/blog/ja/tdiary.conf"
-    conf = Pathname(conf).expand_path
-    if conf.exist?
-      blog_base_dir = Pathname(dir).expand_path
-      tdiary_conf_dir = conf.dirname
-      break
-    end
+def main
+  sf_user = nil
+  parser = OptionParser.new
+  parser.on("--sf-user=[USER]", "Specify sf.net user") do |user|
+    sf_user = user
   end
-  tdiary_compiled_dir = (blog_base_dir + "blog-html/ja").to_s
+
+  begin
+    parser.parse!
+  rescue OptionParser::ParseError => ex
+    $stderr.puts ex.message
+    $stderr.puts parser.help
+    exit 1
+  end
+
+  tdiary_compiled_dir = ARGV.shift
+  if tdiary_compiled_dir.nil?
+    tdiary_conf_dir = nil
+    ["~/public_html/milter-manager"].each do |dir|
+      conf = "#{dir}/blog/ja/tdiary.conf"
+      conf = Pathname(conf).expand_path
+      if conf.exist?
+        blog_base_dir = Pathname(dir).expand_path
+        tdiary_conf_dir = conf.dirname
+        break
+      end
+    end
+    tdiary_compiled_dir = (blog_base_dir + "blog-html/ja").to_s
+  end
+
+  sf_project = "milter-manager"
+  sf_host = "web.sourceforge.net"
+  sf_htdocs_path = "/home/groups/m/mi/milter-manager/htdocs"
+  system("echo", "rsync", "-avz", "--delete", tdiary_compiled_dir,
+         "#{sf_user},#{sf_project}@#{sf_host}:#{sf_htdocs_path}/blog/")
 end
 
-sf_user = "ktou"
-sf_project = "milter-manager"
-sf_host = "web.sourceforge.net"
-sf_htdocs_path = "/home/groups/m/mi/milter-manager/htdocs"
-system("rsync", "-avz", "--delete", tdiary_compiled_dir,
-       "#{sf_user},#{sf_project}@#{sf_host}:#{sf_htdocs_path}/blog/")
+main
