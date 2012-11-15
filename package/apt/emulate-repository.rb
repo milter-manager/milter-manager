f09030a081f3bb7ef12114bd339a7767a3c46a35
Kenji Okimoto <okimoto@clear-code.com>
2012-11-15 14:10:14 +0900

    package apt: add utility script to emulate repository server

---
+++ package/apt/emulate-repository.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+#
+# How to run
+#
+# Edit /etc/hosts as following:
+#
+# /etc/hosts:
+#   ...
+#   # add an entry to specify emulation server
+#   192.168.1.5 downloads.sourceforge.net
+#   ...
+#
+# Run server:
+#   $ sudo ./emurate-repository.rb
+#
+
+require "webrick"
+
+server = WEBrick::HTTPServer.new(:DocumentRoot => "./",
+                                 :BindAddress => "0.0.0.0",
+                                 :Port => 80)
+
+server.mount("/project/milter-manager/", WEBrick::HTTPServlet::FileHandler, "./")
+trap("INT"){ server.shutdown }
+server.start
