11b78e90be85f9f64a673d20f0f0fd5d8a70e0fb
Kenji Okimoto <okimoto@clear-code.com>
2012-11-21 12:35:58 +0900

    package yum: Copy milter-manager-release*.rpm to CentOS 6 directory

--- package/yum/build-release-rpm.sh
+++ package/yum/build-release-rpm.sh
@@ -84,9 +84,14 @@ EOR
     run mkdir -p $top_dir
     run cp -p $rpm_base_dir/RPMS/noarch/${PACKAGE}-release-* $top_dir
     run cp -p $rpm_base_dir/SRPMS/${PACKAGE}-release-* $top_dir
+
     run cp -p $rpm_base_dir/RPMS/noarch/${PACKAGE}-release-* $top_dir/5/stable/i386/Packages/
     run cp -p $rpm_base_dir/RPMS/noarch/${PACKAGE}-release-* $top_dir/5/stable/x86_64/Packages/
     run cp -p $rpm_base_dir/SRPMS/${PACKAGE}-release-* $top_dir/5/stable/SRPMS/
+
+    run cp -p $rpm_base_dir/RPMS/noarch/${PACKAGE}-release-* $top_dir/6/stable/i386/Packages/
+    run cp -p $rpm_base_dir/RPMS/noarch/${PACKAGE}-release-* $top_dir/6/stable/x86_64/Packages/
+    run cp -p $rpm_base_dir/SRPMS/${PACKAGE}-release-* $top_dir/6/stable/SRPMS/
 
     run cp -p ${script_base_dir}/RPM-GPG-KEY-${PACKAGE} $top_dir
 done
