77a5df6f17020ec6a9e986b78b2cbf20380c941f
Kenji Okimoto <okimoto@clear-code.com>
2013-06-13 15:22:55 +0900

    binding ruby: Replace glib-1.1.3 with glib-2.0.2

---
+++ binding/ruby/glib-2.0.2/lib/gnome2/rake/external-package.rb
@@ -0,0 +1,198 @@
+# -*- ruby -*-
+#
+# Copyright (C) 2013  Ruby-GNOME2 Project Team
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+module GNOME2
+  module Rake
+    class ExternalPackage < Struct.new(:name,
+                                       :label,
+                                       :version,
+                                       :download_site,
+                                       :download_base_url,
+                                       :compression_method,
+                                       :windows,
+                                       :native,
+                                       :patches,
+                                       :need_autogen,
+                                       :need_autoreconf,
+                                       :build_concurrently,
+                                       :bundled_packages)
+      def initialize(properties)
+        super()
+        properties.each do |key, value|
+          send("#{key}=", value)
+        end
+      end
+
+      def compression_method
+        super || "gz"
+      end
+
+      def base_name
+        "#{name}-#{version}"
+      end
+
+      def archive_base_name
+        "#{base_name}.tar.#{compression_method}"
+      end
+
+      def archive_url
+        "#{download_base_url}/#{archive_base_name}"
+      end
+
+      def download_base_url
+        super || download_site_base_url
+      end
+
+      def patches
+        super || []
+      end
+
+      def need_autogen?
+        need_autogen
+      end
+
+      def need_autoreconf?
+        need_autoreconf
+      end
+
+      def windows
+        super || WindowsConfiguration.new({})
+      end
+
+      def windows=(properties)
+        super(WindowsConfiguration.new(properties))
+      end
+
+      def native
+        super || NativeConfiguration.new({})
+      end
+
+      def native=(properties)
+        super(NativeConfiguration.new(properties))
+      end
+
+      def bundled_packages
+        super || []
+      end
+
+      private
+      def download_site_base_url
+        case download_site
+        when :gnome
+          base_url = "http://ftp.gnome.org/pub/gnome/sources"
+          release_series = version.gsub(/\A(\d+\.\d+).+\z/, '\1')
+          base_url << "/#{name}/#{release_series}"
+        else
+          base_url = nil
+        end
+        base_url
+      end
+
+      class WindowsConfiguration < Struct.new(:build,
+                                              :include_paths,
+                                              :library_paths,
+                                              :configure_args,
+                                              :patches,
+                                              :need_autogen,
+                                              :need_autoreconf,
+                                              :build_concurrently,
+                                              :use_cc_environment_variable)
+        def initialize(properties)
+          super()
+          properties.each do |key, value|
+            send("#{key}=", value)
+          end
+        end
+
+        def build?
+          build.nil? ? true : build
+        end
+
+        def include_paths
+          super || []
+        end
+
+        def library_paths
+          super || []
+        end
+
+        def configure_args
+          super || []
+        end
+
+        def patches
+          super || []
+        end
+
+        def need_autogen?
+          need_autogen.nil? ? false : need_autogen
+        end
+
+        def need_autoreconf?
+          need_autoreconf.nil? ? false : need_autoreconf
+        end
+
+        def build_concurrently?
+          build_concurrently.nil? ? true : build_concurrently
+        end
+
+        def use_cc_environment_variable?
+          use_cc_environment_variable.nil? ? true : use_cc_environment_variable
+        end
+      end
+
+      class NativeConfiguration < Struct.new(:build,
+                                             :configure_args,
+                                             :patches,
+                                             :need_autogen,
+                                             :need_autoreconf,
+                                             :build_concurrently)
+        def initialize(properties)
+          super()
+          properties.each do |key, value|
+            send("#{key}=", value)
+          end
+        end
+
+        def build?
+          build.nil? ? false : build
+        end
+
+        def configure_args
+          super || []
+        end
+
+        def patches
+          super || []
+        end
+
+        def need_autogen?
+          need_autogen.nil? ? false : need_autogen
+        end
+
+        def need_autoreconf?
+          need_autoreconf.nil? ? false : need_autoreconf
+        end
+
+        def build_concurrently?
+          build_concurrently.nil? ? true : build_concurrently
+        end
+      end
+    end
+  end
+end
---
+++ binding/ruby/glib-2.0.2/lib/gnome2/rake/package-task.rb
@@ -0,0 +1,248 @@
+# coding: utf-8
+
+# Copyright(C) 2011 Ruby-GNOME2 Project.
+#
+# This program is licenced under the same license of Ruby-GNOME2.
+
+require "find"
+require "pathname"
+
+require "rubygems"
+require "rubygems/package_task"
+require "rake/extensiontask"
+require "gnome2/rake/package"
+require "gnome2/rake/external-package"
+require "gnome2/rake/source-download-task"
+require "gnome2/rake/native-binary-build-task"
+require "gnome2/rake/win32-binary-download-task"
+require "gnome2/rake/win32-binary-build-task"
+
+module GNOME2
+  module Rake
+    class PackageTask
+      include ::Rake::DSL
+
+      attr_accessor :name, :summary, :description, :author, :email, :homepage, :required_ruby_version, :post_install_message
+      attr_reader :root_dir
+      def initialize
+        initialize_variables
+        initialize_configurations
+        file, line, method = caller[1].scan(/^(.*):(\d+)(?::.*`(.*)')?\Z/).first
+        @package = Package.new(File.dirname(file))
+        @packages = FileList["#{@package.root_dir.parent}/*"].map{|f| File.directory?(f) ? File.basename(f) : nil}.compact
+        @name = @package.name
+        @cross_compiling_hooks = []
+        yield(self) if block_given?
+      end
+
+      def cross_compiling(&block)
+        @cross_compiling_hooks << block
+      end
+
+      def define
+        task :default => :build
+        define_spec
+        define_source_tasks
+        define_native_tasks
+        define_win32_tasks
+        define_package_tasks
+      end
+
+      # Deprecated. Use #define instead.
+      def define_tasks
+        define
+      end
+
+      def ruby_gnome2_package?(name)
+        @packages.include?(name)
+      end
+
+      def dependency
+        @dependency_configuration
+      end
+
+      def package
+        @package
+      end
+
+      def windows
+        @package.windows
+      end
+
+      # For backward compatibility
+      def win32
+        windows
+      end
+
+      def native
+        @package.native
+      end
+
+      def version
+        ENV["VERSION"] || guess_version
+      end
+
+      def guess_version
+        versions = {}
+        File.open("#{@package.glib2_root_dir}/ext/glib2/rbglib.h") do |rbglib_h|
+          rbglib_h.each_line do |line|
+            if /#define\s+RBGLIB_([A-Z]+)_VERSION\s+(\d+)/ =~ line
+              versions[$1.downcase] = $2.to_i
+            end
+          end
+        end
+        ["major", "minor", "micro"].collect {|type| versions[type]}.compact.join(".")
+      end
+
+      def external_packages=(packages)
+        @package.external_packages = packages
+      end
+
+      private
+      def initialize_variables
+        @summary = ""
+        @description = ""
+        @author = "The Ruby-GNOME2 Project Team"
+        @email = "ruby-gnome2-devel-en@lists.sourceforge.net"
+        @homepage = "http://ruby-gnome2.sourceforge.jp/"
+        @external_packages = []
+      end
+
+      def initialize_configurations
+        @dependency_configuration = DependencyConfiguration.new(self)
+      end
+
+      def define_spec
+        @spec = Gem::Specification.new do |s|
+          s.name                  = @name
+          s.summary               = @summary
+          s.description           = @description
+          s.author                = @author
+          s.email                 = @email
+          s.homepage              = @homepage
+          s.version               = version
+          extensions              = FileList["ext/#{@name}/extconf.rb"]
+          extensions.existing!
+          s.extensions            = extensions
+          s.require_paths         = ["lib"]
+          files                   = FileList["ChangeLog", "README",
+                                             "Rakefile", "extconf.rb",
+                                             "lib/**/*.rb",
+                                             "{ext,sample,test,test-unit}/**/*"]
+          files.existing!
+          s.files                 = files
+          s.required_ruby_version = @required_ruby_version || ">= 1.8.5"
+          s.post_install_message  = @post_install_message
+          @dependency_configuration.apply(s)
+        end
+      end
+
+      def define_source_tasks
+        define_source_download_tasks
+      end
+
+      def define_source_download_tasks
+        task = SourceDownloadTask.new(@package)
+        task.define
+      end
+
+      def define_native_tasks
+        define_native_build_tasks
+      end
+
+      def define_native_build_tasks
+        task = NativeBinaryBuildTask.new(@package)
+        task.define
+      end
+
+      def define_win32_tasks
+        define_win32_extension_task
+        define_win32_download_task
+        define_win32_build_task
+      end
+
+      def so_base_name
+        @name.gsub(/-/, "_")
+      end
+
+      def define_win32_extension_task
+        ::Rake::ExtensionTask.new(so_base_name, @spec) do |ext|
+          ext.ext_dir = "ext/#{@name}"
+          ext.cross_compile = true
+          ext.cross_compiling do |spec|
+            if /mingw|mswin/ =~ spec.platform.to_s
+              win32_binary_dir = @package.windows.relative_binary_dir
+              win32_files = []
+              if win32_binary_dir.exist?
+                Find.find(win32_binary_dir.to_s) do |file|
+                  next if /\.zip\z/ =~ file
+                  win32_files << file
+                end
+              end
+              spec.files += win32_files
+            end
+            @cross_compiling_hooks.each do |hook|
+              hook.call(spec)
+            end
+          end
+        end
+
+        def define_win32_download_task
+          GNOME2Win32BinaryDownloadTask.new(@package)
+        end
+
+        def define_win32_build_task
+          GNOME2Win32BinaryBuildTask.new(@package)
+        end
+      end
+
+      def define_package_tasks
+        Gem::PackageTask.new(@spec) do |pkg|
+        end
+      end
+
+      class DependencyConfiguration
+        attr_accessor :platform, :ruby
+        def initialize(package)
+          @package = package
+          @platform = Gem::Platform::RUBY
+          @gem_configuration = GemConfiguration.new(@package)
+        end
+
+        def gem
+          @gem_configuration
+        end
+
+        def apply(spec)
+          spec.platform = @platform
+          @gem_configuration.apply(spec)
+        end
+
+        class GemConfiguration
+          attr_accessor :runtime, :development
+          def initialize(package)
+            @package = package
+            @runtime = []
+            @development = []
+          end
+
+          def apply(spec)
+            @runtime.each do |dependency|
+              spec.add_runtime_dependency(*append_version(dependency))
+            end
+
+            @development.each do |dependency|
+              spec.add_development_dependency(*append_version(dependency))
+            end
+          end
+
+          def append_version(dependency)
+            name, *ver = dependency.is_a?(Array) ? dependency : [dependency]
+            ver << "= #{@package.version}" if @package.ruby_gnome2_package?(name)
+            [name, *ver]
+          end
+        end
+      end
+    end
+  end
+end
