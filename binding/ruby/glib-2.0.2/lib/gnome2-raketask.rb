c722290c8feba63e642a3d4a53b38e52881caf82
Kenji Okimoto <okimoto@clear-code.com>
2014-05-14 17:34:23 +0900

    binding ruby: Remove ruby-glib2 2.0.2 files

--- binding/ruby/glib-2.0.2/lib/gnome2-raketask.rb
+++
@@ -1,8 +0,0 @@
-# Copyright(C) 2013 Ruby-GNOME2 Project.
-#
-# This program is licenced under the same license of Ruby-GNOME2.
-
-# Just for backward compatibility.
-require "gnome2/rake/package-task"
-
-GNOME2Package = GNOME2::Rake::PackageTask
