540012de664cba4c5005c5773ce8d6724e6241cc
Kenji Okimoto <okimoto@clear-code.com>
2014-05-27 13:19:21 +0900

    binding ruby: Add milter/client/testing.rb for test

---
+++ binding/ruby/lib/milter/client/testing.rb
@@ -0,0 +1,56 @@
+module Milter
+  module TestClient
+    def spawn_milter(milter_path, *args)
+      default_options = {
+        :out => "/dev/null",
+        :err => "/dev/null",
+      }
+      if args.last.is_a?(Hash)
+        options = args.pop
+      else
+        options = {}
+      end
+      options = default_options.merge(options)
+
+      default_env = {
+        "MILTER_ENV" => "test"
+      }
+      if options[:env]
+        env = options.delete(:env)
+      else
+        env = {}
+      end
+      env = default_env.merge(env)
+
+      port = options.delete(:port) || 20025
+      host = options.delete(:host) || "localhost"
+      spec = "inet:#{port}@#{host}"
+
+      pid = spawn(env,
+                  RbConfig.ruby,
+                  milter_path,
+                  "--connection-spec=#{spec}",
+                  *args,
+                  options)
+
+      start = Time.now
+      Process.detach(pid)
+      loop do
+        break if Time.now - start > 5
+        begin
+          TCPSocket.new(host, port)
+          break
+        rescue SystemCallError
+        end
+      end
+      pid
+    end
+
+    def kill_milter(pid)
+      Process.kill(:INT, pid)
+      Process.kill(:KILL, pid)
+      Process.waitpid(pid)
+    rescue SystemCallError
+    end
+  end
+end
