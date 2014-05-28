4f62a63915d66a3b75a770a706fd8ff43fb89736
Kenji Okimoto <okimoto@clear-code.com>
2014-05-28 10:30:35 +0900

    binding ruby: Fix API for test
    Before: User must manage PID of milter.
        class TestSomeMilter < Test::Unit::TestCase
          def setup
            @pid = spawn_milter(milter_path, *args)
          end
          def teardown
            kill_milter(@pid)
          end
        end
    
    After: User don't need to manager PID of milter.
        class TestSomeMilter < Test::Unit::TestCase
          def setup
            @milter_runner = Milter::Client::Test::MilterRunner.new(milter_path, *args)
            @milter_runner.run
          end
          def teardown
            @milter_runner.stop
          end
        end

--- binding/ruby/lib/milter/client/testing.rb
+++ binding/ruby/lib/milter/client/testing.rb
@@ -1,45 +1,51 @@
 module Milter
-  module TestClient
-    def spawn_milter(milter_path, *args)
-      if args.last.is_a?(Hash)
-        options = args.pop
-      else
-        options = {}
-      end
+  class Client
+    module Test
+      class MilterRunner
+        def initialize(milter_path, *args)
+          if args.last.is_a?(Hash)
+            @options = args.pop
+          else
+            @options = {}
+          end
 
-      if options[:env]
-        env = options.delete(:env)
-      else
-        env = {}
-      end
+          if options[:env]
+            @env = options.delete(:env)
+          else
+            @env = {}
+          end
 
-      port = options.delete(:port) || 20025
-      host = options.delete(:host) || "localhost"
-      spec = "inet:#{port}@#{host}"
+          port = options.delete(:port) || 20025
+          host = options.delete(:host) || "localhost"
+          @spec = "inet:#{port}@#{host}"
+        end
 
-      pid = spawn(env,
-                  RbConfig.ruby,
-                  milter_path,
-                  "--connection-spec=#{spec}",
-                  *args,
-                  options)
+        def run
+          @pid = spawn(env,
+                       RbConfig.ruby,
+                       milter_path,
+                       "--connection-spec=#{@spec}",
+                       *args,
+                       options)
 
-      start = Time.now
-      Process.detach(pid)
-      loop do
-        break if Time.now - start > 5
-        begin
-          TCPSocket.new(host, port)
-          break
+          start = Time.now
+          Process.detach(pid)
+          loop do
+            break if Time.now - start > 5
+            begin
+              TCPSocket.new(host, port)
+              break
+            rescue SystemCallError
+            end
+          end
+          @pid
+        end
+
+        def stop
+          Process.kill(:KILL, @pid)
         rescue SystemCallError
         end
       end
-      pid
-    end
-
-    def kill_milter(pid)
-      Process.kill(:KILL, pid)
-    rescue SystemCallError
     end
   end
 end
