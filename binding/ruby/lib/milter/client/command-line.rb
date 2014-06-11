0d10f8eda2408fbed0aa529598bd2739bcd35364
Kenji Okimoto <okimoto@clear-code.com>
2014-06-11 15:14:15 +0900

    binding ruby: Add signal handler for each worker processes

--- binding/ruby/lib/milter/client/command-line.rb
+++ binding/ruby/lib/milter/client/command-line.rb
@@ -293,6 +293,13 @@ module Milter
           end
         end
 
+        client.signal_connect("worker-created") do
+          add_signal_handler(client, :HUP) {client.reload}
+          add_signal_handler(client, :USR1) {Milter::Logger.default.reopen}
+          add_signal_handler(client, :INT,  :once => true) {client.shutdown}
+          add_signal_handler(client, :TERM, :once => true) {client.shutdown}
+        end
+
         client.signal_connect("workers-created") do
           add_signal_handler(client, :HUP) do
             client.reload
