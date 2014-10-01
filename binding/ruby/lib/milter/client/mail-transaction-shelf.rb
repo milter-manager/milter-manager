fd59c1a40f0d2a80e228cf182f6b69627fa977aa
Kenji Okimoto <okimoto@clear-code.com>
2014-10-01 15:31:18 +0900

    binding ruby: Serialize as YAML
    GitHub: ref #42

--- binding/ruby/lib/milter/client/mail-transaction-shelf.rb
+++ binding/ruby/lib/milter/client/mail-transaction-shelf.rb
@@ -9,11 +9,12 @@ module Milter
       end
 
       def [](key)
-        @context.get_mail_transaction_shelf_value(key)
+        value = @context.get_mail_transaction_shelf_value(key)
+        YAML.load(value)
       end
 
       def []=(key, value)
-        @context.set_mail_transaction_shelf_value(key, value)
+        @context.set_mail_transaction_shelf_value(key, value.to_yaml)
       end
 
       def each(&block)
