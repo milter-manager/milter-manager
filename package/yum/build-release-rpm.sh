bd9043aa8e29e0503e3ca7ebe9e4cde705e8a208
Hiroshi Ohkubo <ohkubo.fs@gmail.com>
2016-05-08 08:20:53 +0900

    package yum: Drop CentOS 5 support and add CentOS 7 support

--- package/yum/build-release-rpm.sh
+++ package/yum/build-release-rpm.sh
@@ -86,19 +86,19 @@ EOR
     run cp -p $rpm_base_dir/RPMS/noarch/${PACKAGE}-release-* $top_dir
     run cp -p $rpm_base_dir/SRPMS/${PACKAGE}-release-* $top_dir
 
-    run mkdir -p $top_dir/5/stable/i386/Packages/
-    run mkdir -p $top_dir/5/stable/x86_64/Packages/
-    run mkdir -p $top_dir/5/stable/source/SRPMS/
-    run cp -p $rpm_base_dir/RPMS/noarch/${PACKAGE}-release-* $top_dir/5/stable/i386/Packages/
-    run cp -p $rpm_base_dir/RPMS/noarch/${PACKAGE}-release-* $top_dir/5/stable/x86_64/Packages/
-    run cp -p $rpm_base_dir/SRPMS/${PACKAGE}-release-* $top_dir/5/stable/source/SRPMS/
-
     run mkdir -p $top_dir/6/stable/i386/Packages/
     run mkdir -p $top_dir/6/stable/x86_64/Packages/
     run mkdir -p $top_dir/6/stable/source/SRPMS/
     run cp -p $rpm_base_dir/RPMS/noarch/${PACKAGE}-release-* $top_dir/6/stable/i386/Packages/
     run cp -p $rpm_base_dir/RPMS/noarch/${PACKAGE}-release-* $top_dir/6/stable/x86_64/Packages/
     run cp -p $rpm_base_dir/SRPMS/${PACKAGE}-release-* $top_dir/6/stable/source/SRPMS/
+
+    run mkdir -p $top_dir/7/stable/i386/Packages/
+    run mkdir -p $top_dir/7/stable/x86_64/Packages/
+    run mkdir -p $top_dir/7/stable/source/SRPMS/
+    run cp -p $rpm_base_dir/RPMS/noarch/${PACKAGE}-release-* $top_dir/7/stable/i386/Packages/
+    run cp -p $rpm_base_dir/RPMS/noarch/${PACKAGE}-release-* $top_dir/7/stable/x86_64/Packages/
+    run cp -p $rpm_base_dir/SRPMS/${PACKAGE}-release-* $top_dir/7/stable/source/SRPMS/
 
     run cp -p ${script_base_dir}/RPM-GPG-KEY-${PACKAGE} $top_dir
 done
