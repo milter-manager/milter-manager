06616f869dd4a6c6ec94eae19bc3a22b72c86273
Kouhei Sutou <kou@clear-code.com>
2016-05-11 18:47:31 +0900

    ruby: use double quote for string literal

--- binding/ruby/lib/milter/manager/redhat-detector.rb
+++ binding/ruby/lib/milter/manager/redhat-detector.rb
@@ -13,8 +13,8 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this library.  If not, see <http://www.gnu.org/licenses/>.
 
-require 'milter/manager/redhat-init-detector'
-require 'milter/manager/redhat-upstart-detector'
+require "milter/manager/redhat-init-detector"
+require "milter/manager/redhat-upstart-detector"
 
 module Milter::Manager
   class RedHatDetector
