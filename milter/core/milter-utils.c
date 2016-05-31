0bb6396db5633c503f1d21e9d2239eb77b2b43e5
Kouhei Sutou <kou@clear-code.com>
2016-05-31 21:33:07 +0900

    Enable color on TERM=xterm-256color by default

--- milter/core/milter-utils.c
+++ milter/core/milter-utils.c
@@ -767,6 +767,7 @@ milter_utils_guess_console_color_usability (void)
     term = g_getenv("TERM");
     if (term && (g_str_has_suffix(term, "term") ||
                  g_str_has_suffix(term, "term-color") ||
+                 g_str_has_suffix(term, "term-256color") ||
                  g_str_equal(term, "screen") ||
                  g_str_equal(term, "linux")))
         return TRUE;
