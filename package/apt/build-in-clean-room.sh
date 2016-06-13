14577adeb3f30a1802dd48303b53601e6e497e2a
Kenji Okimoto <okimoto@clear-code.com>
2016-06-13 13:13:15 +0900

    package apt: Precise does not support systemd

--- package/apt/build-in-clean-room.sh
+++ package/apt/build-in-clean-room.sh
@@ -107,10 +107,12 @@ build_by_pbuilder()
                 -e 's/ruby-dev (>= 1:1.9.3)/ruby1.9.1-dev (>= 1.9.1)/g' \
                 -e '/ruby-gnome2-dev/d' \
                 -e '/ruby-glib2/d' \
+                -e '/dh-systemd/d' \
                 $builddir/${PACKAGE}-${VERSION}/debian/control
             sed -i \
                 -e '/--enable-ruby-milter/i \\t\t--with-ruby=/usr/bin/ruby1.9.1 \\' \
                 -e 's/ruby -rrbconfig/ruby1.9.1 -rrbconfig/g' \
+                -e 's/ --with=systemd//' \
                 $builddir/${PACKAGE}-${VERSION}/debian/rules
             sed -i -e 's,^# ,,' $builddir/${PACKAGE}-${VERSION}/debian/ruby-milter-core.install
             ;;
