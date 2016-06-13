dd21f0335866d2fa04d093e835e44f773ce6f61a
Kenji Okimoto <okimoto@clear-code.com>
2016-06-13 13:13:28 +0900

    package apt: Support xenial

--- package/apt/build-in-clean-room.sh
+++ package/apt/build-in-clean-room.sh
@@ -68,7 +68,7 @@ build_by_pbuilder()
     OPTS+=( --basetgz "$basetgz" )
     OPTS+=( --aptcache "$aptcache_dir" )
     case $code_name in
-        precise|trusty|vivid|wily)
+        precise|trusty|wily|xenial)
             OPTS+=( --components 'main universe' )
             MIRROR=http://jp.archive.ubuntu.com/ubuntu
             OPTS+=( --mirror "$MIRROR" )
