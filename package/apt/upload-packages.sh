76e4550e547b9364b36ccb9a908de610f86b68d4
Kenji Okimoto <okimoto@clear-code.com>
2016-11-02 16:07:43 +0900

    pacakge apt: Replace upload task to use packagecloud
    Remove download task because we don't download all packages from sf.net.

---
+++ package/apt/upload-packages.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+# -*- indent-tabs-mode: nil; sh-basic-offset: 4; sh-indentation: 4 -*-
+
+if [ $# != 3 ]; then
+    echo "Usage: $0 VERSION RELEASE ARCHITECTURES CODES"
+    echo "Usage: $0 2.1.0 1 'i386 amd64' 'jessie stretch unstable'"
+    exit 1
+fi
+
+PACKAGE=milter-manager
+VERSION=$1
+RELEASE=$2
+ARCHITECTURES=$3
+CODES=$4
+
+script_base_dir=$(dirname $(realpath -e $0))
+
+run()
+{
+    "$@"
+    if test $? -ne 0; then
+        echo "Failed $@"
+        exit 1
+    fi
+}
+
+upload_packages()
+{
+    architecture=$1
+    code_name=$2
+
+    case ${code_name} in
+        jessie|stretch|unstable)
+            distribution=debian
+            component=main
+            ;;
+    esac
+    status=stable
+    package_initial=$(echo ${PACKAGE} | sed -e 's/\(.\).*/\1/')
+    pool_dir=${script_base_dir}/${distribution}/${status}/pool
+    pool_dir=${pool_dir}/${code_name}/${component}/${package_initial}/${PACKAGE}
+
+    for package in $(sed -ne "/^Files:/,$ p" $pool_dir/${PACKAGE}_${VERSION}-${RELEASE}_${architecture}.changes | grep -E "(\.deb|\.dsc)$" | cut -d" " -f6); do
+        if test -f $package; then
+            package_cloud push $PACKAGE/repos/${distribution}/${code_name} $pool_dir/$package
+        else
+            echo "Missing: ${package}"
+        fi
+    done
+}
+
+for architecture in $ARCHITECTURES; do
+    for code_name in $CODES; do
+        upload_packages $architecture $code_name
+    done
+done
