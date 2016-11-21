77c3cc442be78756ed11027c3ccd063d6d10daf2
Kenji Okimoto <okimoto@clear-code.com>
2016-11-21 17:18:45 +0900

    package/yum: Fix typos

--- package/yum/upload-packages.sh
+++ package/yum/upload-packages.sh
@@ -7,7 +7,7 @@ PROJECT=$3
 VERSION=$4
 RELEASE=$5
 
-for v in "$CENTOS_VERSIONS"; do
-    find ${DISTRIBUTION} -name "*-${VERSION}-${RELEASE}*.rpm" | \
-        xargs -n 1 pacakge_cloud push $PROJECT/repos/el/${v}
+for v in ${CENTOS_VERSIONS}; do
+    find ${DISTRIBUTION}/${v} -name "*-${VERSION}-${RELEASE}*.rpm" | \
+        xargs -n 1 package_cloud push $PROJECT/repos/el/${v}
 done
