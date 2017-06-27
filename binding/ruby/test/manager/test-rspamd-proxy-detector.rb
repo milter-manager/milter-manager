622068ef734cc53078a628b6dbba43956268fac9
Kouhei Sutou <kou@clear-code.com>
2017-06-28 00:20:19 +0900

    test: they are stub not mock

--- binding/ruby/test/manager/test-rspamd-proxy-detector.rb
+++ binding/ruby/test/manager/test-rspamd-proxy-detector.rb
@@ -3,8 +3,8 @@ class TestRspamdProxyDetector < Test::Unit::TestCase
        host: ["mail.example.com:11332", "inet:11332@mail.example.com"])
   test "detect" do |(bind_socket, expected)|
     detector = ::Milter::Manager::RspamdProxyDetector.new
-    mock(File).executable?("/usr/bin/rspamadm") { true }
-    mock(detector).__double_definition_create__.call("`".to_sym, "/usr/bin/rspamadm configdump --json") do
+    stub(File).executable?("/usr/bin/rspamadm") { true }
+    stub(detector).__double_definition_create__.call("`".to_sym, "/usr/bin/rspamadm configdump --json") do
       {
         "worker" => [
           {
