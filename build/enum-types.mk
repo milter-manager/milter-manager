e69acead042a92b0ce65c51daba28953d8057338
Sutou Kouhei <kou@clear-code.com>
2022-07-22 16:06:02 +0900

    Use file for glib-mkenums

--- build/enum-types.mk
+++ build/enum-types.mk
@@ -13,23 +13,20 @@ CLEANFILES +=					\
 	stamp-$(enum_source_prefix)-h
 
 enum_types_mk = $(top_srcdir)/build/enum-types.mk
+enum_types_template_c = $(top_srcdir)/build/enum-types-template.c
+enum_types_template_h = $(top_srcdir)/build/enum-types-template.h
 
 $(enum_source_prefix).c: stamp-$(enum_source_prefix)-c $(enum_source_prefix).h
 	@true
 
-stamp-$(enum_source_prefix)-c: $(enum_sources_h) $(enum_types_mk)
+stamp-$(enum_source_prefix)-c: $(enum_sources_h) $(enum_types_mk) $(enum_types_template_c)
 	$(AM_V_GEN) \
+	sed -e 's/@enum_types_header@/$(enum_source_prefix).h/g' \
+	  $(enum_types_template_c) > enum-types-template.c && \
+	build_enum_types_template_c=$(abs_builddir)/enum-types-template.c && \
 	(cd $(srcdir) && \
-	  include_headers="" && \
-	  for h in $(enum_sources_h); do \
-	    include_headers="$${include_headers}#include \"$${h}\"\n"; \
-	  done && \
 	  $(GLIB_MKENUMS) \
-	    --fhead "#include \"$(enum_source_prefix).h\"\n$${include_headers}" \
-	    --fprod "\n/* enumerations from \"@filename@\" */" \
-	    --vhead "GType\n@enum_name@_get_type (void)\n{\n  static GType etype = 0;\n  if (etype == 0) {\n    static const G@Type@Value values[] = {" 	\
-	    --vprod "      { @VALUENAME@, \"@VALUENAME@\", \"@valuenick@\" }," \
-	    --vtail "      { 0, NULL, NULL }\n    };\n    etype = g_@type@_register_static (\"@EnumName@\", values);\n  }\n  return etype;\n}\n" \
+	    --template $${build_enum_types_template_c} \
 	    $(enum_sources_h)) > tmp-$(enum_source_prefix).c && \
 	(cmp -s tmp-$(enum_source_prefix).c $(enum_source_prefix).c || \
 	  cp tmp-$(enum_source_prefix).c $(enum_source_prefix).c ) && \
@@ -39,14 +36,11 @@ stamp-$(enum_source_prefix)-c: $(enum_sources_h) $(enum_types_mk)
 $(enum_source_prefix).h: stamp-$(enum_source_prefix)-h
 	@true
 
-stamp-$(enum_source_prefix)-h: $(enum_sources_h) $(enum_types_mk)
-	$(AM_V_GEN) (cd $(srcdir) && \
-	  mark="__`echo $(enum_source_prefix) | sed -e 's/-/_/g' | tr a-z A-Z`_H__" && \
+stamp-$(enum_source_prefix)-h: $(enum_sources_h) $(enum_types_mk) $(enum_types_template_h)
+	$(AM_V_GEN) \
+	(cd $(srcdir) && \
 	  $(GLIB_MKENUMS) \
-	    --fhead "#ifndef $${mark}\n#define $${mark}\n\n#include <glib-object.h>\n\nG_BEGIN_DECLS\n" \
-	    --fprod "/* enumerations from \"@filename@\" */\n" \
-	    --vhead "GType @enum_name@_get_type (void);\n#define MILTER_TYPE_@ENUMSHORT@ (@enum_name@_get_type())\n" 	\
-	    --ftail "G_END_DECLS\n\n#endif /* $${mark} */" \
+	    --template $(enum_types_template_h) \
 	    $(enum_sources_h)) > tmp-$(enum_source_prefix).h && \
 	(cmp -s tmp-$(enum_source_prefix).h $(enum_source_prefix).h || \
 	  cp tmp-$(enum_source_prefix).h $(enum_source_prefix).h) && \
