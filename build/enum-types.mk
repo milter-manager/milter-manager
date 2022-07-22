12fc5b0775d5ceab113b4d32b1263d044af6bd97
Sutou Kouhei <kou@clear-code.com>
2022-07-22 16:26:33 +0900

    Use absolute path for enum-types-template.h

--- build/enum-types.mk
+++ build/enum-types.mk
@@ -15,6 +15,7 @@ CLEANFILES +=					\
 enum_types_mk = $(top_srcdir)/build/enum-types.mk
 enum_types_template_c = $(top_srcdir)/build/enum-types-template.c
 enum_types_template_h = $(top_srcdir)/build/enum-types-template.h
+abs_enum_types_template_h = $(abs_top_srcdir)/build/enum-types-template.h
 
 $(enum_source_prefix).c: stamp-$(enum_source_prefix)-c $(enum_source_prefix).h
 	@true
@@ -23,10 +24,10 @@ stamp-$(enum_source_prefix)-c: $(enum_sources_h) $(enum_types_mk) $(enum_types_t
 	$(AM_V_GEN) \
 	sed -e 's/@enum_types_header@/$(enum_source_prefix).h/g' \
 	  $(enum_types_template_c) > enum-types-template.c && \
-	build_enum_types_template_c=$(abs_builddir)/enum-types-template.c && \
+	abs_build_enum_types_template_c=$(abs_builddir)/enum-types-template.c && \
 	(cd $(srcdir) && \
 	  $(GLIB_MKENUMS) \
-	    --template $${build_enum_types_template_c} \
+	    --template $${abs_build_enum_types_template_c} \
 	    $(enum_sources_h)) > tmp-$(enum_source_prefix).c && \
 	(cmp -s tmp-$(enum_source_prefix).c $(enum_source_prefix).c || \
 	  cp tmp-$(enum_source_prefix).c $(enum_source_prefix).c ) && \
@@ -40,7 +41,7 @@ stamp-$(enum_source_prefix)-h: $(enum_sources_h) $(enum_types_mk) $(enum_types_t
 	$(AM_V_GEN) \
 	(cd $(srcdir) && \
 	  $(GLIB_MKENUMS) \
-	    --template $(enum_types_template_h) \
+	    --template $(abs_enum_types_template_h) \
 	    $(enum_sources_h)) > tmp-$(enum_source_prefix).h && \
 	(cmp -s tmp-$(enum_source_prefix).h $(enum_source_prefix).h || \
 	  cp tmp-$(enum_source_prefix).h $(enum_source_prefix).h) && \
