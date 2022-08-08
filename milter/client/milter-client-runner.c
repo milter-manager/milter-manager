e1c7228276266fff0e0dec960d1186ce91811f01
Sutou Kouhei <kou@clear-code.com>
2022-08-08 14:26:31 +0900

    client: add document for milter_client_runner_get_client()
        milter/client/milter-client-runner.h:55: Warning: MilterClient:
        milter_client_runner_get_client: return value: Missing (transfer)
        annotation

--- milter/client/milter-client-runner.c
+++ milter/client/milter-client-runner.c
@@ -1,6 +1,6 @@
 /* -*- Mode: C; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 /*
- *  Copyright (C) 2011  Kouhei Sutou <kou@clear-code.com>
+ *  Copyright (C) 2011-2022  Sutou Kouhei <kou@clear-code.com>
  *
  *  This library is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License as published by
@@ -165,6 +165,12 @@ milter_client_runner_quit (MilterClientRunner *runner)
     runner_class->quit(runner);
 }
 
+/**
+ * milter_client_runner_get_client:
+ * @runner: A #MilterClientRunner.
+ *
+ * Returns: (transfer none): The #MilterClient of the runner.
+ */
 MilterClient *
 milter_client_runner_get_client (MilterClientRunner *runner)
 {
